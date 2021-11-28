ad0066a032ee8dd84f2446b821268efc026f794e
Nur Sahirah <nursahirah@icloud.com>
2021-11-28 16:53:35 +0800

    Coding

--- Application/src/components/Admin/AdminDashboard/AppConfiguration/PendingAppConfigList/index.js
+++ Application/src/components/Admin/AdminDashboard/AppConfiguration/PendingAppConfigList/index.js
@@ -2,36 +2,35 @@ import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 
 class index extends Component {
-    render() {
-        return ( 
-          <div> 
-         <div className="container">
+  render() {
+    return (
+      <div>
+        <div className="container">
           <div className="row">
-         <div className="col order-first">
-          Pending Configuration Application
-    </div>
-    </div>
-    </div>
+            <div className="col order-first">
+              Pending Configuration Application
+            </div>
+          </div>
+        </div>
 
 
-    <div>  
-      <a href="https://wwww.alphaoil.com" > 
-      1.  AlphaOil
-      </a>
-       </div>
+        <div>
+          <a href="https://wwww.alphaoil.com" >
+            1.  AlphaOil
+          </a>
+        </div>
 
-        <div>  
-      <a href="https://wwww.petronasup.com" > 
-      2.  PetronasUp
-      </a>
-       </div>
+        <div>
+          <a href="https://wwww.petronasup.com" >
+            2.  PetronasUp
+          </a>
+        </div>
 
-       </div>
-        );
-    }
+      </div>
+    );
+  }
 }
 
 export default index;
 
-  
-    
+

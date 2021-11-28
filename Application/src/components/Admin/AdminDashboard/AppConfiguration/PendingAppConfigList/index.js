3d81ebf83f132973772e263ef2b727a20f228ba1
Nur Sahirah <nursahirah@icloud.com>
2021-11-28 16:46:28 +0800

    test test

--- Application/src/components/Admin/AdminDashboard/AppConfiguration/PendingAppConfigList/index.js
+++ Application/src/components/Admin/AdminDashboard/AppConfiguration/PendingAppConfigList/index.js
@@ -1,6 +1,37 @@
-import react from 
-<div class="container">
-  <div class="row">
-    <div class="col order-first">
-      Pending Configuration Application
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+
+class index extends Component {
+    render() {
+        return ( 
+          <div> 
+         <div className="container">
+          <div className="row">
+         <div className="col order-first">
+          Pending Configuration Application
     </div>
+    </div>
+    </div>
+
+
+    <div>  
+      <a href="https://wwww.alphaoil.com" > 
+      1.  AlphaOil
+      </a>
+       </div>
+
+        <div>  
+      <a href="https://wwww.petronasup.com" > 
+      2.  PetronasUp
+      </a>
+       </div>
+
+       </div>
+        );
+    }
+}
+
+export default index;
+
+  
+    

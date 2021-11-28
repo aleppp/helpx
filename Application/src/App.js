da0d9b9ff477e82bf1ca7c6469ff818da9c47e66
Nur Sahirah Mohd Daud <nursahirah.mdaud@petronas.com>
2021-11-28 16:50:57 +0000

    Updated App.js

--- Application/src/App.js
+++ Application/src/App.js
@@ -1,12 +1,2 @@
-import './App.css';
-import PendingAppConfigList from "./components/Admin/AdminDashboard/AppConfiguration/PendingAppConfigList";
-function App() {
-  return (
-    <div className="App">
-      <PendingAppConfigList />
-    </div>
-  );
-}
 
-export default App;
 

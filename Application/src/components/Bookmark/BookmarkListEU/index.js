ce683a8dcb03707076e22d7aceb00b31a62bcbd9
Haizan Azman <norhaizan.norazman@petronas.com>
2021-11-29 14:49:24 +0800

    Added bookmarked and unbookmark icon. Also made some modification to index.js on BookmarkListEU

--- Application/src/components/Bookmark/BookmarkListEU/index.js
+++ Application/src/components/Bookmark/BookmarkListEU/index.js
@@ -1,1 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
 
+function BookmarkIcon(props) {
+  const userbookmark = this.props.userbookmark;
+  if (userbookmark == 1) {
+    return <Bookmarked />;
+  }
+  return <Unbookmarked />;
+}
+
+function Bookmarked() {
+  return <img src={linkwithopaquebookmarkicon} />;
+}
+
+function Unbookmarked() {
+  return <img src={linkwithseethroughbookmarkicon} onClick={BookmarkPopup} />;
+}
+
+ReactDOM.render(<BookmarkIcon />, document.getElementById("root"));

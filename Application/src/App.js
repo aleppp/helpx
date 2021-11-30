f0babfe4e5f0de60d120018c84c48fe46e967ac3
Nur Sahirah <nursahirah@icloud.com>
2021-11-30 12:21:54 +0800

    updated App.js

--- Application/src/App.js
+++ Application/src/App.js
@@ -1,12 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
-import PendingAppConfigList from "./components/Admin/AdminDashboard/AppConfiguration/PendingAppConfigList";
+
 function App() {
-  return (
-    <div className="App">
-      <PendingAppConfigList />
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
-

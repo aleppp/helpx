938c1241d9f213da23705f2ba4f14773d2b3f29d
Ahmad Shahmy <shahmyansary@gmail.com>
2021-12-01 16:53:31 +0800

    341258 Updated index. js under details folder

---
+++ Application/src/components/ReleaseNotes/Details/index.js
@@ -0,0 +1,69 @@
+import "./components/ReleaseNotes/Details/index.css";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import SwitchUnstyled from "@mui/material/Switch";
+import DateRangeOutlinedIcon from "@mui/icons-material/DateRangeOutlined";
+
+const label = { componentsProps: { input: { "aria-label": "Demo switch" } } };
+
+function index() {
+  return (
+    <div>
+      <div className="container">
+        <h2>Details</h2>
+
+        <button className="darkgreen" onclick="preview()">
+          Preview
+        </button>
+
+        <div className="word">
+          <SwitchUnstyled {...label} size="small" />
+          <label htmlFor="Visibility"> Visibility </label>
+          <div className="tooltip">
+            <HelpOutlineIcon fontSize="extrasmall" />
+            <span className="tooltiptext">
+              <p>Visible On: End user can view the content</p>
+              <br />
+              Visible Off: Only Content creator, Content approval and Admin can
+              view this content
+            </span>
+          </div>
+        </div>
+
+        <div className="word">
+          <SwitchUnstyled {...label} size="small" />
+          <label htmlFor="Show Feedback Button"> Show Feedback Button </label>
+          <div className="tooltip">
+            <HelpOutlineIcon fontSize="extarasmall" />
+            <span className="tooltiptext">
+              <p>Feedback On: End user can give feedback about the content</p>
+              <br />
+              Feedback Off: End user cannot give feedback about the content
+            </span>
+          </div>
+        </div>
+
+        <div className="word">
+          <div className="calander">
+            <DateRangeOutlinedIcon />
+          </div>
+          <label htmlFor="Schedule">Schedule: 30/2/2022 </label>
+          <a href="www.editlink.com">Edit</a>
+        </div>
+      </div>
+
+      <div className="buttonContainer">
+        <button className="blue" onclick="sendForApproval()">
+          Send for Approval
+        </button>
+        <button className="red" onclick="delete()">
+          Delete
+        </button>
+        <button className="green" onclick="saveAsDraft()">
+          Save as Draft
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default index;

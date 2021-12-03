c7e3f3af14a1d8eb781db43bc7d728d95f9bbf19
Nurul Izzah binti Sukhaidi <nurulizzah.sukhaidi@petronas.com>
2021-12-03 13:13:41 +0800

    341247 Added component for normal editor under Release Noted  without CSS file

---
+++ Application/src/components/ReleaseNotes/Editor/NormalEditor.js
@@ -0,0 +1,42 @@
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import React, { useState } from "react";
+import SpellcheckIcon from "@mui/icons-material/Spellcheck";
+//import "./App.css";
+
+function NormalEditor() {
+  const clicked = () => {};
+  const [text, setText] = useState("");
+  const [title, setTitle] = useState("");
+  return (
+    <div className="App">
+      <div className="background">
+        <button className="button-usetemplate" onClick={clicked}>
+          Use Template
+        </button>
+        <h3 className="h3">Title </h3>
+        <form>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </form>
+        <SpellcheckIcon className="spell-icon" />
+        <div className="editor">
+          <h3 className="h3">Body Content</h3>
+          <CKEditor
+            editor={ClassicEditor}
+            data={text}
+            onChange={(event, editor) => {
+              const data = editor.getData();
+              setText(data);
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NormalEditor;

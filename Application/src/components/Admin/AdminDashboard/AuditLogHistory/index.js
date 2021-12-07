4f1b9f8ad5d72f4d92e25c2ed56c6cd23eef517f
Nurul Anisah Mohamad Asri <nurulanisah.mohamada@petronas.com>
2021-12-07 23:23:20 +0000

    Updated index.js changes of table styling

--- Application/src/components/Admin/AdminDashboard/AuditLogHistory/index.js
+++ Application/src/components/Admin/AdminDashboard/AuditLogHistory/index.js
@@ -1,35 +1,72 @@
 import React from "react";
 import "./style.css";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 function AuditLogHistory() {
   return (
-    <div class="AuditLogHistory">
-      <h1>Audit Log</h1>
+    <div>
+      <div className="audit-1">
+        <h1>Audit Log History</h1>
+        <img
+          src={process.env.PUBLIC_URL + "/images/export.png"}
+          className="export"
+        />
+      </div>
 
       <table>
         <tr>
-          <th>Date & Time</th>
-          <th>User</th>
-          <th>Category</th>
-          <th>Changes</th>
-          <th>Changed Object</th>
-          <th>Action</th>
+          <th>
+            Date & Time <ExpandMoreIcon />
+          </th>
+          <th>
+            User <ExpandMoreIcon />
+          </th>
+          <th>
+            {" "}
+            Category <ExpandMoreIcon />{" "}
+          </th>
+          <th>
+            {" "}
+            Changes <ExpandMoreIcon />{" "}
+          </th>
+          <th>
+            {" "}
+            Changed Object <ExpandMoreIcon />{" "}
+          </th>
+          <th> Action </th>
         </tr>
-
         <tr>
-          <td>1/11/2021 9:00PM</td>
-          <td>Admin 1</td>
-          <td>User</td>
-          <td>Assign Role</td>
-          <td>Balqis</td>
+          <td> 1/11/2021 9:00PM </td>
+          <td> Admin 1 </td>
+          <td> User </td>
+          <td> Assign Role </td>
+          <td> Balqis </td>
           <a href="a">Show More</a>
         </tr>
         <tr>
-          <td>1/11/2021 9:21PM</td>
-          <td>Admin 2</td>
-          <td>Application</td>
-          <td>New Content Added</td>
-          <td>Alpha Oil</td>
+          <td> 1/11/2021 9:20PM </td>
+          <td> Admin 2 </td>
+          <td> Application </td>
+          <td> Change Database </td>
+          <td> Amir </td>
+          <a href="a">Show More</a>
+        </tr>
+
+        <tr>
+          <td> 1/11/2021 9:420PM </td>
+          <td> Admin 3 </td>
+          <td> User </td>
+          <td> Delete User </td>
+          <td> Nisa </td>
+          <a href="a">Show More</a>
+        </tr>
+
+        <tr>
+          <td> 2/11/2021 10:01AM </td>
+          <td> Admin 3 </td>
+          <td> Application </td>
+          <td> Add New Application </td>
+          <td> Setel </td>
           <a href="a">Show More</a>
         </tr>
       </table>

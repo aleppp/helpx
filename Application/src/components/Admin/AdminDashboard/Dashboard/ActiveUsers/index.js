be50fda82dd1a26c52e75c3d2ec6249a9395872d
Balqis Batrisyia Mohd Nizam <balqisbatrisyia.mniz@petronas.com>
2021-12-07 02:53:03 +0000

    Updated index.js

---
+++ Application/src/components/Admin/AdminDashboard/Dashboard/ActiveUsers/index.js
@@ -0,0 +1,14 @@
+// index.js under Active Users
+
+import React from "react";
+import "./style.css";
+
+function ActiveUsers(props) {
+  return (
+    <div className="ActiveUsers">
+      <p> Active Users 250</p>
+    </div>
+  );
+}
+
+export default ActiveUsers;

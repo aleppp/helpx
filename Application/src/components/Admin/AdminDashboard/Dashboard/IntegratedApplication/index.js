c2acd4a7901261aaa2a54920b3c4c9b2905ef0fa
Balqis Batrisyia Mohd Nizam <balqisbatrisyia.mniz@petronas.com>
2021-12-07 02:53:34 +0000

    Updated index.js for integrated application

---
+++ Application/src/components/Admin/AdminDashboard/Dashboard/IntegratedApplication/index.js
@@ -0,0 +1,14 @@
+// index.js under IntegratedApplication
+
+import React from "react";
+import "./style.css";
+
+function IntegratedApplications(props) {
+  return (
+    <div className="IntegratedApp">
+      <p> Integrated Application 7</p>
+    </div>
+  );
+}
+
+export default IntegratedApplications;

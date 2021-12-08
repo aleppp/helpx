b85ae51a95ded2a769d31cfb0590237adef7a611
Chua <farahjannahch@gmail.com>
2021-12-09 02:16:54 +0800

    Added ContentBody for release notes

---
+++ Application/src/components/ReleaseNotes/ContentBody/ContentBody.js
@@ -0,0 +1,86 @@
+import "./ContentBody.css";
+
+function ContentBody() {
+  return (
+    <>
+      <div class="center">
+        <div className="scroll">
+          <div className="page-header">
+            <h1>
+              <strong>Release Note 6.0</strong>
+            </h1>
+          </div>
+          <div className="list-content">
+            <ul>
+              <li class="contents-h2">
+                <a href="#content1">What is New</a>
+              </li>
+              <li class="contents-h2">
+                <a href="#content2">Purpose of the Change</a>
+              </li>
+              <li class="contents-h2">
+                <a href="#content3">How are you affected</a>
+              </li>
+              <li class="contents-h2">
+                <a href="#content4">Action needed</a>
+              </li>
+              <li class="contents-h2">
+                <a href="#content5">History</a>
+              </li>
+            </ul>
+          </div>
+          <div className="note">
+            <h2 id="content1">What is New</h2>
+            <p>Gives some descriptions</p>
+            <h2 id="content2">Purpose of the Change</h2>
+            <p>
+              Release notes are documents that people receive when they buy
+              and/or download software products. Nowadays, they're often
+              distributed virtually. In the modern world, release notes are more
+              important than ever. This is because buying software products
+              isn't as straightforward a process as it used to be. Buying
+              software products is now an ongoing process as you can count on
+              the fact that you'll be sent product updates, bug fixes, new
+              features, and more after your initial purchase.
+            </p>
+            <h2 id="content3">How are you Affected</h2>
+            <p>
+              Release notes are documents that people receive when they buy
+              and/or download software products. Nowadays, they're often
+              distributed virtually. In the modern world, release notes are more
+              important than ever. This is because buying software products
+              isn't as straightforward a process as it used to be. Buying
+              software products is now an ongoing process as you can count on
+              the fact that you'll be sent product updates, bug fixes, new
+              features, and more after your initial purchase.
+            </p>
+            <h2 id="content4">Action needed</h2>
+            <p>
+              Release notes are documents that people receive when they buy
+              and/or download software products. Nowadays, they're often
+              distributed virtually. In the modern world, release notes are more
+              important than ever. This is because buying software products
+              isn't as straightforward a process as it used to be. Buying
+              software products is now an ongoing process as you can count on
+              the fact that you'll be sent product updates, bug fixes, new
+              features, and more after your initial purchase.
+            </p>
+            <h2 id="content5">History</h2>
+            <p>
+              Release notes are documents that people receive when they buy
+              and/or download software products. Nowadays, they're often
+              distributed virtually. In the modern world, release notes are more
+              important than ever. This is because buying software products
+              isn't as straightforward a process as it used to be. Buying
+              software products is now an ongoing process as you can count on
+              the fact that you'll be sent product updates, bug fixes, new
+              features, and more after your initial purchase.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ContentBody;

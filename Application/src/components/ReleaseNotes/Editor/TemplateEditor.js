e60f628c985a894b3e2e3b7bcc19de6d59a3c73c
Najihah Rosli <nurnajihah.rosli@petronas.com>
2021-12-09 00:56:35 +0800

    Updated TemplateEditor.js under Editor

--- Application/src/components/ReleaseNotes/Editor/TemplateEditor.js
+++ Application/src/components/ReleaseNotes/Editor/TemplateEditor.js
@@ -1,8 +1,8 @@
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
-import React from "react";
+import React, { useState } from "react";
 import "./TemplateEditor.css";
-import TemplatePopup from "./TemplatePopup";
+import TemplatePopup from "./TemplatePopup.js";
 
 const TEMPLATES = [
   { name: "Template 1.0" },
@@ -11,10 +11,22 @@ const TEMPLATES = [
 ];
 
 function TemplateEditor() {
+  const [name, setName] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="App">
       <div className="editor-background">
-        <button className="button-newtemplate"> Save as New Template</button>
+        <input
+          type="button"
+          value="Save As New Template"
+          className="button-newtemplate"
+          onClick={togglePopup}
+        />
         <div>
           <form>
             <select className="template">
@@ -35,19 +47,42 @@ function TemplateEditor() {
           alt="spell-icon"
         />
         <h3 className="h3">Body Content</h3>
-        <CKEditor
-          editor={ClassicEditor}
-          data="<h2>What is New</h2>
+        <div>
+          <CKEditor
+            editor={ClassicEditor}
+            data="<h2>What is New</h2>
                 <p>Insert a lot of text here. Here  is the edited form for the detailed discussion or question that asked by enduser.  Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum. At nam minimum ponderum. Est audiam animal molestiae te. Ex duo eripuit mentitum.</p>
                 <h2>Purpose of The Change</h2>
                 <p>Insert a lot of text here. Here  is the edited form for the detailed discussion or question that asked by enduser.  Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum. At nam minimum ponderum. Est audiam animal molestiae te. Ex duo eripuit mentitum.</p>
                 "
-        />
-        <button className="button-delete">Delete</button>
-        <button className="button-update">Update</button>
-        <button className="button-draft">Save to Draft</button>
+          />
+          <div />
+        </div>
+        {isOpen && (
+          <TemplatePopup
+            content={
+              <>
+                <h2> New Template </h2>
+                <form>
+                  <label className="label">Name</label>
+                  <input
+                    className="name-field"
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </form>{" "}
+                <div className="actions">
+                  <button className="button-save">Save</button>
+
+                  <button className="button-cancel">Cancel</button>
+                </div>
+              </>
+            }
+            handleClose={togglePopup}
+          />
+        )}
       </div>
-      <TemplatePopup />
     </div>
   );
 }

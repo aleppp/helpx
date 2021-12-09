f3e720690ec841247c12c1d424d42db6bb7f3b51
Nurul Syahirah Mokhtar <nurulsyahirah.mokhta@petronas.com>
2021-12-09 02:21:35 +0000

    Deleted faqnew.test.js

--- Application/src/components/FAQ/FAQNew/faqnew.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import FAQNew from "./";
-
-it("render without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<FAQNew />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});

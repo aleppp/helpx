90270f334830d510fc451f389356ca3e35dee9fa
Alif Muqri Hazmi <alifmuqri.hazmi@petronas.com>
2021-12-09 02:03:07 +0000

    354741 - Add scripts for edit FAQ slider for admin FAQ list data table

---
+++ Application/src/components/Admin/AdminFAQList/ActionButtonsFAQTable/EditFAQ/EditFAQ.js
@@ -0,0 +1,69 @@
+import React from "react";
+import "../style.css";
+import Buttons from "../../../../Buttons/Buttons";
+
+export default function EditFAQ() {
+  const button = [
+    {
+      type: "button-red",
+      text: "Delete",
+    },
+    {
+      type: "button-green",
+      text: "Edit",
+    },
+  ];
+
+  return (
+    <div class="container-addnew-admin">
+      <h2 className="title-addfaq">Edit FAQ</h2>
+      <div>
+        <p className="float-left">FAQ01</p>
+        <label class="switch">
+          <input type="checkbox"></input>
+          <span class="slider round"></span>
+        </label>
+        <p id="visible">Visibility</p>
+      </div>
+      <br />
+      <br />
+      <br />
+      <div className="display-row">
+        <label className="section-label" for="faqsection">
+          FAQ Section
+        </label>
+        <select className="select-section" id="faqsection" name="faqsection">
+          <option value="ao">Alpha Oil</option>
+          <option value="pd">Petronas Digital</option>
+          <option value="pu">Petronas Up</option>
+          <option value="se">Setel</option>
+        </select>
+
+        <label for="faqorder">FAQ Order</label>
+        <select className="select-order" id="faqorder" name="faqorder">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+        </select>
+      </div>
+      <label className="float-left" for="question">
+        Question
+      </label>
+      <textarea id="question" name="question" placeholder="Enter a question.">
+        What is the contents of release notes?
+      </textarea>
+      <label className="float-left" for="answer">
+        Answer
+      </label>
+      <textarea id="answer" name="answer" placeholder="Enter an answer.">
+        Release notes contains the changes, bugs fixed, and documentation on
+        uses.
+      </textarea>
+      <div className="button-float">
+        <Buttons button={button[0]}></Buttons>
+        <Buttons button={button[1]}></Buttons>
+      </div>
+    </div>
+  );
+}

02ff238972ff2feeb3e39b5cb943812644e64a9d
M Hazman Nasrullah Sukiman <mhazmannasrullah.suk@petronas.com>
2021-12-10 02:29:36 +0000

    Updated App.js

--- Application/src/App.js
+++ Application/src/App.js
@@ -1,12 +1,33 @@
+import { Route, Redirect } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./components/LandingPage/LandingPage";
+import Auth from "./Services/Auth/Auth";
+import { Component } from "react";
+import Header from "./components/LandingPage/Header/Header";
+import Callback from "./Services/Auth/Callback";
+import ListOfReleaseNotes from "./components/UserHomepage/UserHomepage";
 
-function App() {
-  return (
-    <div className="App">
-      <LandingPage />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.auth = new Auth(this.props.history);
+  }
+
+  render() {
+    return (
+      <>
+        <Header auth={this.auth} />
+        <div className="App">
+          <Route
+            path="/"
+            exact
+            render={(props) => <LandingPage auth={this.auth} {...props} />}
+          />
+          <Route path="/UserHomepage" component={ListOfReleaseNotes} />
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;

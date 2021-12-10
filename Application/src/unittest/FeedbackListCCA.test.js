81f1f6ebc1a1079e9d5a4e0f0aa7b511217cc703
Nur Adriana Syaza Noor Azmi <nuradrianasyaza.noor@petronas.com>
2021-12-10 07:01:34 +0000

    Updated FeedbackListCCA.test.js

--- Application/src/unittest/FeedbackListCCA.test.js
+++ Application/src/unittest/FeedbackListCCA.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "../components/unittest/FeedbackListCCA.test.js";
+import App from "../components/Feedback/FeedbackListCCA/index.js";
 
 it("render without crashing", () => {
   const div = document.createElement("div");

53f277f03c4e0ce9bf34cf7cffe7f1f2e799521c
M Hazman Nasrullah Sukiman <mhazmannasrullah.suk@petronas.com>
2021-12-10 02:28:53 +0000

    Updated LandingPage.js

--- Application/src/components/LandingPage/LandingPage.js
+++ Application/src/components/LandingPage/LandingPage.js
@@ -1,29 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
+import { Route, Redirect } from "react-router-dom";
 import "./LandingPage.css";
 import VerticalBar from "./VerticalBar/VerticalBar";
 import Header from "./Header/Header";
 import HorizontalBar from "./HorizontalBar/HorizontalBar";
 import Menu from "./Menu/Menu";
+import Auth from "../../Services/Auth/Auth";
 
-function LandingPage() {
-  return (
-    <div className="container">
-      <div className="nav">
-        <VerticalBar />
+class LandingPage extends Component {
+  render() {
+    const { isAuthenticated, login } = this.props.auth;
+
+    return (
+      <div className="container">
+        <div className="nav">
+          <VerticalBar />
+        </div>
+        <div className="body">
+          <div className="header">
+            <Route
+              path="/"
+              exact
+              render={(props) => <Header auth={this.auth} {...props} />}
+            />
+          </div>
+          <div className="navhor">
+            <HorizontalBar />
+          </div>
+          <div className="menu">
+            {isAuthenticated() ? <p>Sucess</p> : <Menu />}
+          </div>
+        </div>
       </div>
-      <div className="body">
-        <div className="header">
-          <Header />
-        </div>
-        <div className="navhor">
-          <HorizontalBar />
-        </div>
-        <div className="menu">
-          <Menu />
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default LandingPage;

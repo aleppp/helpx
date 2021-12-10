1825716e204e74e13bd732485f2e9e3ae0012dd3
Nur Adriana Syaza Noor Azmi <nuradrianasyaza.noor@petronas.com>
2021-12-10 05:55:40 +0000

    Updated index.js on Word Filter List

--- Application/src/components/FraudManagement/WordFilterList/index.js
+++ Application/src/components/FraudManagement/WordFilterList/index.js
@@ -1,1 +1,62 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function App() {
+  return (
+    <>
+      <div className="container">
+        <h1>Word Filter List</h1>
+        <table className="table table-borderless text-center">
+          <thead>
+            <tr>
+              <td colSpan={2}></td>
+            </tr>
+            <tr>
+              <th>ID</th>
+              <th>Term</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {data.map((item) => {
+              return (
+                <tr key={item.ID}>
+                  <td>{item.ID}</td>
+                  <td>{item.Term}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
+
+const data = [
+  {
+    ID: "WFL1",
+    Term: "Bodo",
+  },
+
+  {
+    ID: "WFL2",
+    Term: "Bodoh",
+  },
+
+  {
+    ID: "WFL3",
+    Term: "Bodohhhhh",
+  },
+
+  {
+    ID: "WFL4",
+    Term: "Bodoooooo",
+  },
+
+  {
+    ID: "WFL5",
+    Term: "Bodohh",
+  },
+];
 

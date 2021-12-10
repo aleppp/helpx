faf989b9b6274592fbdd05d5ecbc1f1857c0a93c
M Hazman Nasrullah Sukiman <mhazmannasrullah.suk@petronas.com>
2021-12-10 02:28:21 +0000

    Updated Header.js

--- Application/src/components/LandingPage/Header/Header.js
+++ Application/src/components/LandingPage/Header/Header.js
@@ -1,27 +1,25 @@
-import React from "react";
+import { React, Component } from "react";
 import "./Header.css";
 import "../../Buttons/Buttons.css";
-import Buttons from "../../Buttons/Buttons";
+import { Link } from "react-router-dom";
 
-const Button = [
-  {
-    type: "button-green",
-    text: "Login",
-  },
-];
-
-function Header() {
-  return (
-    <div className="canvas">
-      <div className="container-1">
-        <div className="col-1">Helpx</div>
-        <div className="col-2"></div>
-        <div className="col-3">
-          <Buttons button={Button[0]}></Buttons>
+class Header extends Component() {
+  render() {
+    const { isAuthenticated, login, logout } = this.props.auth;
+    return (
+      <div className="canvas">
+        <div className="container-1">
+          <div className="col-1">Helpx</div>
+          <div className="col-2"></div>
+          <div className="col-3">
+            <button onClick={isAuthenticated() ? logout : login}>
+              {isAuthenticated() ? "Log Out" : "Log In"}
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Header;

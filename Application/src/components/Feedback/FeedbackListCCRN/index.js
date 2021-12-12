94472584fa964eb45886c139a7234d95c52fad40
Fatin Nurliana Zainal <fatinnurliana.zainal@petronas.com>
2021-12-12 09:52:10 +0000

    Updated index.js for FeedbakListCCRN

--- Application/src/components/Feedback/FeedbackListCCRN/index.js
+++ Application/src/components/Feedback/FeedbackListCCRN/index.js
@@ -1,60 +1,54 @@
-import * as React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./index.css";
-import "./index.js";
-import VeryBad from "./images/rate_1.png";
-import Bad from "./images/rate_2.png";
-import Average from "./images/rate_3.png";
-import Good from "./images/rate_4.png";
-import VeryGood from "./images/rate_5.png";
-
+import React from 'react';
 
 function FeedbackListCCRN() {
 
   const FeedbackData = [
     {
       UserName: "Anna Park",
-      Rating: <img src={VeryGood} alt="Excellent" />,
+      Rating: <img src={process.env.PUBLIC_URL + "/images/rate_5.png"}/>,
       Feedback: "The release note is so helpful",
-      Read: <button id = "MarkRead" >Mark As Unread</button>,
-      Date: '09/03/2021 09:08PM'
+      Read: "Mark as Read",
+      Date: '01/12/2021'
     },
 
     {
       UserName: "Sarah binti Abdullah",
-      Rating: <img src={Average} alt="Good" />,
+      Rating: <img src={process.env.PUBLIC_URL + "/images/rate_3.png"}/>,
       Feedback: "I don't really understand about this release note. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      Read: <button id = "MarkRead">Mark As Read</button>,
-      Date: '09/03/2021 09:08PM'
+      Read: "Mark as Unread",
+      Date: '09/03/2021'
     },
 
     {
       UserName: "Ali",
-      Rating: <img src={VeryBad} alt=" Very Bad" />,
+      Rating: <img src={process.env.PUBLIC_URL + "/images/rate_1.png"}/>,
       Feedback: "The release note is too complicated to understand!",
-      Read: <button id = "MarkRead">Mark As Unread</button>,
-      Date: '09/03/2021 09:08PM'
+      Read: "Mark as Read",
+      Date: '18/05/2021'
     },
 
     {
-      UserName: "Wiwawu",
-      Rating: <img src={Bad} alt="Bad" />,
+      UserName: "Aaron",
+      Rating: <img src={process.env.PUBLIC_URL + "/images/rate_2.png"}/>,
       Feedback: "So many things to read.",
-      Read: <button id = "MarkRead">Mark As Read</button>,
-      Date: '09/03/2021 09:08PM'
+      Read: "Mark as Read",
+      Date: '30/10/2021'
     }
 
   ];
+  
 
   return (
     <div class = 'container'>
       <h2><b>Feedback</b></h2>
         <div class = "dropdown">
-          <form id = "DropdownFeedbackCCRN">     
-            <select>
-                <option id = "All" name = "All" selected>All</option>
-                <option id = "Newest" name = "newest">Newest</option>
-                <option id = "Oldest" name = "oldest">Oldest</option>
+          <form name = "DropdownFeedbackCCRN">     
+            <select name = "DropdownList" id = "selectDropdown">
+                <option value = "All" name = "All" selected>All</option>
+                <option value = "Newest" name = "newest">Newest</option>
+                <option value = "Oldest" name = "oldest">Oldest</option>
             </select>
           </form>
 
@@ -65,6 +59,7 @@ function FeedbackListCCRN() {
         <div class = 'feedbackList'>
         
           <><table>
+            <tbody>
                 <tr>
 
                   <th>{FeedbackData.Rating}</th>
@@ -74,16 +69,19 @@ function FeedbackListCCRN() {
                 </tr>
 
                 <tr>
-                  <td colspan="2">{FeedbackData.Feedback}</td>
+                  <td colSpan="2">{FeedbackData.Feedback}</td>
                 </tr>
+                </tbody>
               </table>
               <div class="read">
                   <table>
+                    <tbody>
                     <tr>
-                      <td>{FeedbackData.Read}</td>
+                      <td class = "readdata"><button>{FeedbackData.Read}</button></td>
                       <td><p>{FeedbackData.Date}</p></td>
-                      <td><img src="read_icon.svg"></img></td>
+                      <td><img src={process.env.PUBLIC_URL + "/images/read_icon.png"}></img></td>
                     </tr>
+                    </tbody>
                   </table>
                 </div></>
            
@@ -95,6 +93,4 @@ function FeedbackListCCRN() {
         </div>
     )
 }
-
-
 export default FeedbackListCCRN;

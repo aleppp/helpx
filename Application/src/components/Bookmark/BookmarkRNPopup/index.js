5e09fb33b3e68e4bbe27a1eeaa63c74d3916d427
Nor Haizan Nor Azman <norhaizan.norazman@petronas.com>
2021-12-13 14:36:39 +0000

    Updated index.js in BookmarkRN

--- Application/src/components/Bookmark/BookmarkRNPopup/index.js
+++ Application/src/components/Bookmark/BookmarkRNPopup/index.js
@@ -2,100 +2,81 @@ import React from "react";
 import "./style.css";
 import "reactjs-popup/dist/index.css";
 import Popup from "reactjs-popup";
-import PropTypes from "prop-types";
-import Rating from "@mui/material/Rating";
 
-import Emoji1 from "./Emoji1.png";
-import Emoji2 from "./Emoji2.png";
-import Emoji3 from "./Emoji3.png";
-import Emoji4 from "./Emoji4.png";
-import Emoji5 from "./Emoji5.png";
+function BookmarkIcon() {
+  var userbookmark = 0;
+  //Coding that will fetch bookmark data from database that will determine either the user saved the bookmark or not
+  if (userbookmark === 1) {
+    return <Bookmarked />;
+  } else {
+    return <BookmarkPopup />;
+  }
+}
 
-function FeedbackRN() {
+function Bookmarked() {
   return (
-    <div className="center" data-testid="divTest">
-      <h2>
-        If you find this documentation helpful, please give us a feedback.{" "}
-      </h2>
-      <br />
+    <>
+      <h1>Release Notes </h1>
+      <img src={process.env.PUBLIC_URL + "/icons/bookmarked.svg"} />
+    </>
+  );
+}
 
-      <div>
-        <h3>Rating:</h3>
-        <RadioGroupRating />
-        <h3>Feedback:</h3>
-
-        <input type="text" placeholder="Enter your feedback here" />
-
-        <br />
-        <Popup trigger={<button className="button"> Submit</button>} modal>
+function BookmarkPopup() {
+  return (
+    <>
+      <div data-testid="divTest">
+        <h1>Release Notes</h1>
+        <Popup
+          trigger={
+            <img src={process.env.PUBLIC_URL + "/icons/unbookmarked.svg"} />
+          }
+          modal
+        >
           {(close) => (
             <div className="modal">
+              <div className="header">
+                {" "}
+                Bookmark Added<br></br>{" "}
+              </div>
+
               <div className="content">
                 {" "}
-                Thank you! Your feedback has been recorded
+                <form>
+                  <div id="container1">
+                    <label for="bname">Name : </label>
+                    <input type="text" id="bname" name="bname"></input>
+                  </div>
+                </form>
               </div>
-              <div className="actions"></div>
-              <button
-                className="button"
-                onClick={() => {
-                  console.log("modal closed ");
-                  close();
-                }}
-              >
-                Done
-              </button>
+
+              <div className="actions">
+                <button
+                  className="button2"
+                  onClick={() => {
+                    console.log("modal closed ");
+                    close();
+                  }}
+                >
+                  Cancel
+                </button>
+                <button
+                  className="button1"
+                  onClick={() => {
+                    //Coding here to save Bookmark in database according to release note and the user
+                    console.log("modal closed ");
+                    close();
+                  }}
+                >
+                  Done
+                </button>
+              </div>
             </div>
           )}
         </Popup>
-
-        <br />
       </div>
-    </div>
+    </>
   );
 }
 
-const customIcons = {
-  1: {
-    icon: <img src={Emoji3} alt="3" />,
-    label: "Very Dissatisfied",
-  },
-  2: {
-    icon: <img src={Emoji4} alt="4" />,
-    label: "Dissatisfied",
-  },
-  3: {
-    icon: <img src={Emoji5} alt="5" />,
-    label: "Neutral",
-  },
-  4: {
-    icon: <img src={Emoji1} alt="1" />,
-    label: "Satisfied",
-  },
-  5: {
-    icon: <img src={Emoji2} alt="2" />,
-    label: "Very Satisfied",
-  },
-};
-
-function IconContainer(props) {
-  const { value, ...other } = props;
-  return <span {...other}>{customIcons[value].icon}</span>;
-}
-
-IconContainer.propTypes = {
-  value: PropTypes.number.isRequired,
-};
-
-function RadioGroupRating() {
-  return (
-    <Rating
-      name="highlight-selected-only"
-      defaultValue={2}
-      IconContainerComponent={IconContainer}
-      //highlightSelectedOnly={true}
-      //onChangeActive={true}
-    />
-  );
-}
-
-export default FeedbackRN;
+export default BookmarkIcon;

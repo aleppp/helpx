b21878c81ef99862103946db47db8de1270055fa
Nurul Syahirah Mokhtar <nurulsyahirah.mokhta@petronas.com>
2021-12-13 15:38:42 +0000

    Updated script for FAQNew component.

--- Application/src/components/FAQ/FAQNew/index.js
+++ Application/src/components/FAQ/FAQNew/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./style.css";
-import Button from "../../Buttons/Buttons";
+import Buttons from "../../Buttons/Buttons";
 
-export default function FAQNew() {
+export default function NewFAQ() {
   const button = [
     {
       type: "button-red",
@@ -15,7 +15,8 @@ export default function FAQNew() {
   ];
 
   return (
-    <div class="container">
+    <div class="container-addnew-admin">
+      <h2 className="title-addfaq">Add New FAQ</h2>
       <div>
         <label class="switch">
           <input type="checkbox"></input>
@@ -24,29 +25,49 @@ export default function FAQNew() {
         <p id="visible">Visibility</p>
       </div>
       <br />
-      <label for="faqsection">FAQ Section</label>
-      <select id="faqsection" name="faqsection">
-        <option value="ao">Alpha Oil</option>
-        <option value="pd">Petronas Digital</option>
-        <option value="pu">Petronas Up</option>
-        <option value="se">Setel</option>
-      </select>
-      <label for="question">Question</label>
-      <textarea
-        id="question"
-        name="question"
-        placeholder="Enter a question."
-      ></textarea>
-      <label for="answer">Answer</label>
-      <textarea
-        id="answer"
-        name="answer"
-        placeholder="Enter an answer."
-      ></textarea>
       <br />
-      <Button button={button[0]}></Button>
-      <Button button={button[1]}></Button>
+      <br />
+      <div className="display-row">
+        <label className="section-label" for="faqsection">
+          FAQ Section
+        </label>
+        <select className="select-section" id="faqsection" name="faqsection">
+          <option value="ao">Alpha Oil</option>
+          <option value="pd">Petronas Digital</option>
+          <option value="pu">Petronas Up</option>
+          <option value="se">Setel</option>
+        </select>
+
+        <label for="faqorder">FAQ Order</label>
+        <select className="select-order" id="faqorder" name="faqorder">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+        </select>
+      </div>
+      <div className="display-column">
+        <label className="float-left" for="question">
+          Question
+        </label>
+        <textarea
+          id="question"
+          name="question"
+          placeholder="Enter a question."
+        ></textarea>
+        <label className="float-left" for="answer">
+          Answer
+        </label>
+        <textarea
+          id="answer"
+          name="answer"
+          placeholder="Enter an answer."
+        ></textarea>
+      </div>
+      <div className="button-float">
+        <Buttons button={button[0]}></Buttons>
+        <Buttons button={button[1]}></Buttons>
+      </div>
     </div>
   );
 }
-

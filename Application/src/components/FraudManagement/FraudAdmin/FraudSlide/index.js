25ceab9d8054b5dccce32dd9d327f49a08468121
Noreliza Khusari <noreliza.khusari@petronas.com>
2021-12-19 15:14:46 +0000

    Added index.js for slider under fraud configuration for admin.

---
+++ Application/src/components/FraudManagement/FraudAdmin/FraudSlide/index.js
@@ -0,0 +1,26 @@
+import "./style.css";
+import Button from "../Buttons/Buttons";
+
+export default function App() {
+  const button = [
+    {
+      type: "button-green",
+      text: "Save",
+    },
+    {
+      type: "button-red",
+      text: "Cancel",
+    },
+  ];
+
+  return (
+    <div class="container">
+      <label for="question">Term</label>
+      <textarea id="term" name="term" placeholder="Enter a term."></textarea>
+      <br />
+      <br />
+      <Button button={button[0]}></Button>
+      <Button button={button[1]}></Button>
+    </div>
+  );
+}

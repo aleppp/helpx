c1e0fc4b4f08d82d91b326f949faf5185eb7e96a
Noreliza Khusari <noreliza.khusari@petronas.com>
2021-12-19 15:19:31 +0000

    Updated index.js for admin fraud configuration.

--- Application/src/components/FraudManagement/FraudAdmin/index.js
+++ Application/src/components/FraudManagement/FraudAdmin/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import "./fraud.css";
-import Button from "../Buttons/Buttons";
+import "./style.css";
+import Button from "./button";
 import Pagination from "../Pagination/pagination";
 
 const Buttons = [

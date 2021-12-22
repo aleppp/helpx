859118636c2b61af12b9b9129b8c70ce5bab7d51
Nurqueen Sayang <queensayang123@gmail.com>
2021-12-23 05:32:23 +0800

    add db hooks for list of release notes

--- Application/src/services/MySQL/index.js
+++ Application/src/services/MySQL/index.js
@@ -1,16 +1,58 @@
-const mysql = require("mysql2");
+var mysql = require("mysql");
+const express = require("express");
+const app = express();
+app.use(express.json());
 
-// establish connection to database
-const db = mysql.createConnection({
-  user: "root",
+//connection db
+var con = mysql.createConnection({
   host: "localhost",
+  user: "root",
   password: "helpx",
   database: "helpx",
 });
 
-db.query(`call sp_applications_sel`, (err, result, fields) => {
-  if (err) {
-    return console.log(err);
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+
+  //listener
+  const port = process.env.PORT || 8080;
+  app.listen(port, () => console.log(`Its alive on http://localhost:${port}`));
+
+  // declare functions
+  function getQuery(con, sqlQuery, res) {
+    con.query(sqlQuery, (err, result) => {
+      if (err) {
+        res.send(err.sqlMessage);
+      } else {
+        res.send(result);
+      }
+    });
   }
-  return console.log(result);
+
+  function setQuery(con, sqlQuery, par, res) {
+    con.query(sqlQuery, par, (err, result) => {
+      if (err) {
+        res.send(err.sqlMessage);
+      } else {
+        res.send(result);
+      }
+    });
+  }
+
+  //getReleaseNotes
+  app.get("/releasenotes/list", (req, res) => {
+    const ReleaseNotes = "CALL sp_ReleaseNotes_sel()";
+    getQuery(con, ReleaseNotes, res);
+  });
+
+  /*query
+  if (err) throw err;
+  con.query(
+    "SELECT * FROM lookupappattributes",
+    function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+    }
+  );*/
 });

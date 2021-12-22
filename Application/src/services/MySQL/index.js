23617fba25ee703a98868c0e3c6d958785daeb41
Alif Muqri Hazmi <alifmuqri.hazmi@petronas.com>
2021-12-22 23:19:21 +0800

    add listener and db hook for create new application under admin

--- Application/src/services/MySQL/index.js
+++ Application/src/services/MySQL/index.js
@@ -2,8 +2,12 @@ const mysql = require("mysql2");
 const express = require("express");
 const cors = require("cors");
 const app = express();
-app.use(express.json());
+app.use(express.json()); //middleware to parse json
 app.use(cors());
+const PORT = 8080;
+
+//listener
+app.listen(PORT, () => console.log(`live http://localhost:${PORT}`));
 
 // establish connection to database
 const db = mysql.createConnection({
@@ -42,7 +46,19 @@ function setQuery(db, sqlQuery, par, res) {
 }
 
 //db hook for displaying applications in datatable for admin
-app.get("/apps/sel", (res) => {
+app.get("/apps/sel", (req, res) => {
   const getApplicationsList = "CALL sp_applications_sel()";
   getQuery(db, getApplicationsList, res);
+});
+
+//insert new application
+app.post("/apps/ins", (req, res) => {
+  const insApp = "CALL sp_applications_ins(?,?,?,?)";
+  const params = req.body.apps;
+  setQuery(
+    db,
+    insApp,
+    [params.name, params.url, params.datecreated, params.datemodified],
+    res
+  );
 });

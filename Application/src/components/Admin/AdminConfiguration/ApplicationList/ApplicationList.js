108336c8e7fe4f046118193b37af33d83239a370
Alif Muqri Hazmi <alifmuqri.hazmi@petronas.com>
2021-12-28 12:45:33 +0800

    add component for application list data table

--- Application/src/components/Admin/AdminConfiguration/ApplicationList/ApplicationList.js
+++ Application/src/components/Admin/AdminConfiguration/ApplicationList/ApplicationList.js
@@ -1,31 +1,22 @@
 import Button from "../../../Buttons/Buttons";
-import "./ApplicationSettingsList.css";
+import "./ApplicationList.css";
 
-function ApplicationSettingsList() {
+function ApplicationList() {
   const data = [
     {
+      id: "APP01",
       appName: "AlphaOil",
-      backgroundColor: "Blue",
-      fontSize: "12",
-      fontFamily: "Mulish",
-      theme: "Default",
-      navBar: "Horizontal",
+      AppURL: "petronas.com/alphaoil",
     },
     {
+      id: "APP02",
       appName: "Petronas Up",
-      backgroundColor: "Emerald Green",
-      fontSize: "12",
-      fontFamily: "Mulish",
-      theme: "Monothematic",
-      navBar: "Vertical",
+      AppURL: "petronas.com/petronasup",
     },
     {
+      id: "APP03",
       appName: "Setel",
-      backgroundColor: "Emerald Green",
-      fontSize: "12",
-      fontFamily: "Sans Serif",
-      theme: "Default",
-      navBar: "Vertical",
+      AppURL: "petronas.com/setel",
     },
   ];
   const button = [
@@ -34,60 +25,55 @@ function ApplicationSettingsList() {
       text: "Edit",
     },
     {
+      type: "button-red",
+      text: "Delete",
+    },
+    {
       type: "button-blue",
       text: "Add New",
     },
   ];
   return (
     <div>
-      <div className="app-settings">
-        <h1>Applications Settings</h1>
+      <div className="app-list">
+        <h1>List of Application</h1>
         <div className="button-float-right">
-          <Button button={button[1]}></Button>
-        </div>{" "}
+          <Button button={button[2]} />
+        </div>
       </div>
       <div>
-        <table id="appSettings">
+        <table id="applist">
           <tr>
+            <th>Application ID</th>
             <th>Application Name</th>
-            <th>Background Color</th>
-            <th>Font Size</th>
-            <th>Font Family</th>
-            <th>Theme</th>
-            <th>Navigation Bar</th>
+            <th>Application URL</th>
             <th>Action</th>
           </tr>
           <tr>
+            <td>{data[0].id}</td>
             <td>{data[0].appName}</td>
-            <td>{data[0].backgroundColor}</td>
-            <td>{data[0].fontSize}</td>
-            <td>{data[0].fontFamily}</td>
-            <td>{data[0].theme}</td>
-            <td>{data[0].navBar}</td>
+            <td>{data[0].AppURL}</td>
             <td className="action-column">
               <Button button={button[0]}></Button>
+              <Button button={button[1]}></Button>
             </td>
           </tr>
           <tr>
+            <td>{data[1].id}</td>
             <td>{data[1].appName}</td>
-            <td>{data[1].backgroundColor}</td>
-            <td>{data[1].fontSize}</td>
-            <td>{data[1].fontFamily}</td>
-            <td>{data[1].theme}</td>
-            <td>{data[1].navBar}</td>
+            <td>{data[1].AppURL}</td>
             <td className="action-column">
               <Button button={button[0]}></Button>
+              <Button button={button[1]}></Button>
             </td>
           </tr>
           <tr>
+            <td>{data[2].id}</td>
             <td>{data[2].appName}</td>
-            <td>{data[2].backgroundColor}</td>
-            <td>{data[2].fontSize}</td>
-            <td>{data[2].fontFamily}</td>
-            <td>{data[2].theme}</td>
-            <td>{data[2].navBar}</td>
+            <td>{data[2].AppURL}</td>
             <td className="action-column">
               <Button button={button[0]}></Button>
+              <Button button={button[1]}></Button>
             </td>
           </tr>
         </table>
@@ -96,4 +82,4 @@ function ApplicationSettingsList() {
   );
 }
 
-export default ApplicationSettingsList;
+export default ApplicationList;

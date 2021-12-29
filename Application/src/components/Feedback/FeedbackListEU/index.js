5d0c59437c4e29e6f732700281009a653c0ccac7
Syahirah Mokhtar <syahirahmokhtar7@gmail.com>
2021-12-30 00:22:30 +0800

    Added script for displaying data for feedback list data table.

--- Application/src/components/Feedback/FeedbackListEU/index.js
+++ Application/src/components/Feedback/FeedbackListEU/index.js
@@ -1,36 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./style.css";
 import Button from "../../Buttons/Buttons";
 
-export default function FeedbackList(props) {
-  const data = [
-    {
-      Content: "Release Note 1",
-      Date: "22/10/2021 09:21AM",
-      Feedback: "The release note is so helpful.",
-    },
-    {
-      Content: "Release Note 2",
-      Date: "26/10/2021 21:56PM",
-      Feedback: "I don't understand the content of the release note.",
-    },
-    {
-      Content: "Release Note 3",
-      Date: "02/11/2021 17:34PM",
-      Feedback:
-        "How do I reach out to the admin to give a role to me for editing content?",
-    },
-    {
-      Content: "Release Note 4",
-      Date: "14/11/2021 08:11AM",
-      Feedback: "Is there any modifications made to address the bug?",
-    },
-    {
-      Content: "Release Note 5",
-      Date: "17/11/2021 13:46PM",
-      Feedback: "Whats's the key feature for the latest release note?",
-    },
-  ];
+export default function FeedbackList() {
+  const [FeedbackEUList, setFeedbackEUList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/feedback/sel")
+      .then((res) => {
+        if (res.status === 200) setFeedbackEUList(res.data[0]);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   const button = [
     {
@@ -53,57 +36,28 @@ export default function FeedbackList(props) {
             <tr>
               <th>Content</th>
               <th>Date</th>
+              <th>Rating</th>
               <th>Feedback</th>
               <th>Action</th>
             </tr>
           </thead>
 
           <tbody>
-            <tr>
-              <td>{data[0].Content}</td>
-              <td>{data[0].Date}</td>
-              <td>{data[0].Feedback}</td>
-              <td>
-                <Button button={button[0]}></Button>
-              </td>
-            </tr>
-            <tr>
-              <td>{data[1].Content}</td>
-              <td>{data[1].Date}</td>
-              <td>{data[1].Feedback}</td>
-              <td>
-                <Button button={button[0]}></Button>
-              </td>
-            </tr>
-            <tr>
-              <td>{data[2].Content}</td>
-              <td>{data[2].Date}</td>
-              <td>{data[2].Feedback}</td>
-              <td>
-                <Button button={button[0]}></Button>
-              </td>
-            </tr>
-            <tr>
-              <td>{data[3].Content}</td>
-              <td>{data[3].Date}</td>
-              <td>{data[3].Feedback}</td>
-              <td>
-                <Button button={button[0]}></Button>
-              </td>
-            </tr>
-            <tr>
-              <td>{data[4].Content}</td>
-              <td>{data[4].Date}</td>
-              <td>{data[4].Feedback}</td>
-              <td>
-                <Button button={button[0]}></Button>
-              </td>
-            </tr>
+            {FeedbackEUList.map((fb, i) => (
+              <tr key={i}>
+                <td>{fb.title}</td>
+                <td>{fb.DateCreated}</td>
+                <td>{fb.Rating}</td>
+                <td>{fb.Feedback}</td>
+                <td>
+                  <Button button={button[0]}></Button>
+                </td>
+              </tr>
+            ))}
           </tbody>
-
           <tfoot>
             <tr>
-              <td colSpan={4}>
+              <td colSpan={5}>
                 <p className="foot"></p>
               </td>
             </tr>
@@ -113,4 +67,3 @@ export default function FeedbackList(props) {
     </>
   );
 }
-

19aa61bd4ad43622cd51d9451458e5ed1eb0a745
Balqis Batrisyia Mohd Nizam <balqisbatrisyia.mniz@petronas.com>
2021-12-29 16:32:49 +0000

    add : useeffect and axios to fetch data from api and display in roles management

--- Application/src/components/Admin/AdminManagement/RolesManagement/index.js
+++ Application/src/components/Admin/AdminManagement/RolesManagement/index.js
@@ -1,23 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./style.css";
 import Button from "../../../Buttons/Buttons";
 
-const button = [
-  {
-    type: "button-red",
-    text: "Delete",
-  },
-  {
-    type: "button-blue",
-    text: "Add New",
-  },
-  {
-    type: "button-green",
-    text: "Edit",
-  },
-];
-
 function RolesManagement() {
+  const [RolesManagement, setRolesManagement] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/userroles/sel")
+      .then((res) => {
+        if (res.status === 200) setRolesManagement(res.data[0]);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const button = [
+    {
+      type: "button-red",
+      text: "Delete",
+    },
+    {
+      type: "button-blue",
+      text: "Add New",
+    },
+    {
+      type: "button-green",
+      text: "Edit",
+    },
+  ];
+
   return (
     <div>
       <div className="roles-management-component">
@@ -28,63 +40,35 @@ function RolesManagement() {
       <table>
         <tr>
           <th>
-            Role ID <img
-                className="dropdown"
-                src={process.env.PUBLIC_URL + "/images/expandMore.png"}
-              />
+            Role ID{" "}
+            <img
+              className="dropdown"
+              src={process.env.PUBLIC_URL + "/images/expandMore.png"}
+            />
           </th>
           <th>
-            Role Name <img
-                className="dropdown"
-                src={process.env.PUBLIC_URL + "/images/expandMore.png"}
-              />
+            Role Name{" "}
+            <img
+              className="dropdown"
+              src={process.env.PUBLIC_URL + "/images/expandMore.png"}
+            />
           </th>
           <th> Description </th>
           <th> Number of Users </th>
           <th> Action </th>
         </tr>
-        <tr>
-          <td> R001 </td>
-          <td> Content Approver </td>
-          <td> Approved Content </td>
-          <td> 12 </td>
-          <td>
-            <Button button={button[2]}></Button>
-            <Button button={button[0]}></Button>
-          </td>
-        </tr>
-        <tr>
-          <td> R002 </td>
-          <td> Content Contributor </td>
-          <td> Edit and delete their own posts, but they can not edit or … </td>
-          <td> 07 </td>
-          <td>
-            <Button button={button[2]}></Button>
-            <Button button={button[0]}></Button>
-          </td>
-        </tr>
-
-        <tr>
-          <td> R003 </td>
-          <td> System Admin </td>
-          <td> Administer the content </td>
-          <td> 11 </td>
-          <td>
-            <Button button={button[2]}></Button>
-            <Button button={button[0]}></Button>
-          </td>
-        </tr>
-
-        <tr>
-          <td> R004 </td>
-          <td> End User </td>
-          <td> Consume the content </td>
-          <td> 137 </td>
-          <td>
-            <Button button={button[2]}></Button>
-            <Button button={button[0]}></Button>
-          </td>
-        </tr>
+        {RolesManagement.map((roles, i) => (
+          <tr key={i}>
+            <td>{roles.ID} </td>
+            <td> {roles.Name} </td>
+            <td> {roles.Description} </td>
+            <td> {roles.Number} </td>
+            <td>
+              <Button button={button[2]}></Button>
+              <Button button={button[0]}></Button>
+            </td>
+          </tr>
+        ))}
       </table>
     </div>
   );

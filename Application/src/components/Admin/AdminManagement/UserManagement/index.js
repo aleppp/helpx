abf7ae217975187b30ea22cdb187b18d9275d1f8
Balqis Batrisyia Mohd Nizam <balqisbatrisyia.mniz@petronas.com>
2021-12-29 16:30:19 +0000

    fix : display data table for user management

--- Application/src/components/Admin/AdminManagement/UserManagement/index.js
+++ Application/src/components/Admin/AdminManagement/UserManagement/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./style.css";
 import Button from "../../../Buttons/Buttons";
+
 function UserManagement() {
   const [UserManagement, setUserManagement] = useState([]);
 

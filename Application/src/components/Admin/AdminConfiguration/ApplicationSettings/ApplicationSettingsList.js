49b36bd7a8fa081894a58b19f68f8516c4586239
Alif Muqri Hazmi <alifmuqri.hazmi@petronas.com>
2021-12-29 19:49:19 +0800

    Fetch data from Mysql using useeffect and axios to display application settings data table

--- Application/src/components/Admin/AdminConfiguration/ApplicationSettings/ApplicationSettingsList.js
+++ Application/src/components/Admin/AdminConfiguration/ApplicationSettings/ApplicationSettingsList.js
@@ -1,33 +1,20 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Button from "../../../Buttons/Buttons";
 import "./ApplicationSettingsList.css";
 
 function ApplicationSettingsList() {
-  const data = [
-    {
-      appName: "AlphaOil",
-      backgroundColor: "Blue",
-      fontSize: "12",
-      fontFamily: "Mulish",
-      theme: "Default",
-      navBar: "Horizontal",
-    },
-    {
-      appName: "Petronas Up",
-      backgroundColor: "Emerald Green",
-      fontSize: "12",
-      fontFamily: "Mulish",
-      theme: "Monothematic",
-      navBar: "Vertical",
-    },
-    {
-      appName: "Setel",
-      backgroundColor: "Emerald Green",
-      fontSize: "12",
-      fontFamily: "Sans Serif",
-      theme: "Default",
-      navBar: "Vertical",
-    },
-  ];
+  const [appSettingsList, setAppSettingsList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/appattributes/sel")
+      .then((res) => {
+        if (res.status === 200) setAppSettingsList(res.data[0]);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   const button = [
     {
       type: "button-green",
@@ -48,7 +35,7 @@ function ApplicationSettingsList() {
       </div>
       <div>
         <table id="appSettings">
-          <tr>
+          <thead>
             <th>Application Name</th>
             <th>Background Color</th>
             <th>Font Size</th>
@@ -56,40 +43,22 @@ function ApplicationSettingsList() {
             <th>Theme</th>
             <th>Navigation Bar</th>
             <th>Action</th>
-          </tr>
-          <tr>
-            <td>{data[0].appName}</td>
-            <td>{data[0].backgroundColor}</td>
-            <td>{data[0].fontSize}</td>
-            <td>{data[0].fontFamily}</td>
-            <td>{data[0].theme}</td>
-            <td>{data[0].navBar}</td>
-            <td className="action-column">
-              <Button button={button[0]}></Button>
-            </td>
-          </tr>
-          <tr>
-            <td>{data[1].appName}</td>
-            <td>{data[1].backgroundColor}</td>
-            <td>{data[1].fontSize}</td>
-            <td>{data[1].fontFamily}</td>
-            <td>{data[1].theme}</td>
-            <td>{data[1].navBar}</td>
-            <td className="action-column">
-              <Button button={button[0]}></Button>
-            </td>
-          </tr>
-          <tr>
-            <td>{data[2].appName}</td>
-            <td>{data[2].backgroundColor}</td>
-            <td>{data[2].fontSize}</td>
-            <td>{data[2].fontFamily}</td>
-            <td>{data[2].theme}</td>
-            <td>{data[2].navBar}</td>
-            <td className="action-column">
-              <Button button={button[0]}></Button>
-            </td>
-          </tr>
+          </thead>
+          <tbody>
+            {appSettingsList.map((appSettings, i) => (
+              <tr key={i}>
+                <td>{appSettings.Name}</td>
+                <td>{appSettings.BackgroundColor}</td>
+                <td>{appSettings.FontSize}</td>
+                <td>{appSettings.FontFamily}</td>
+                <td>{appSettings.Theme}</td>
+                <td>{appSettings.NavigationBar}</td>
+                <td className="action-column">
+                  <Button button={button[0]}></Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     </div>

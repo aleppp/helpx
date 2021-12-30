9ca7e53cccd17d6d0eed92c76912f58c9dbb947d
Alif Muqri Hazmi <alifmuqri.hazmi@petronas.com>
2021-12-30 09:44:06 +0800

    fix: Remove all delete sp from Call SP section to prevent data loss

--- SQL Scripts/HelpX_Script.sql
+++ SQL Scripts/HelpX_Script.sql
@@ -2101,7 +2101,6 @@ CALL sp_bookmarks_sel_all();
 CALL sp_template_ins(1,1,'Release Note 1','Here are some details on..', now(), now());
 CALL sp_template_sel();
 CALL sp_template_upd(2,1,1,'Release Note 3.4','Release Note are....',now(),now());
-CALL sp_template_del(1);
 
 CALL sp_auditlogs_sel_byuserid();
 
@@ -2112,25 +2111,17 @@ CALL sp_content_ins(1, 1, 1, 1, true, true, 'Release Note 5.0', 'This is a new R
 
 CALL sp_content_upd(1, 1, true, false, 'Release Note 6.0', 'This is a new Release Notes', now());
 
-CALL sp_content_del(1);
-
 CALL sp_fraudmanagement_sel();
 
 CALL `sp_fraudmanagement_ins`('park yoo', now(), now()) ;
 
 CALL `sp_fraudmanagement_upd`(1, 'crazy', now());
 
-CALL `sp_fraudmanagement_del`(3);
-
-CALL `sp_applications_del`(2);
-
 CALL sp_users_sel() ;
 
 CALL sp_bookmarks_sel_user(2);
 
 CALL sp_users_ins('Roman', 'Kvaska', 'roman.kvaska@gmail.com', now(), now() ) ;
-
-CALL sp_users_del(3);
 
 CALL `sp_applications_upd`(1,'AlphaOil Petronas','alphaoil',now());
 
@@ -2140,7 +2131,6 @@ CALL `sp_faq_sel`();
 
 CALL `sp_feedback_sel_user`();
 
-CALL sp_notifications_del(2);
 
 CALL sp_notifications_sel();
 
@@ -2148,15 +2138,12 @@ CALL sp_bookmarks_ins(2, 'helpx.petronas.com/releasenote/1.11', 'Release Note 1.
 
 CALL sp_bookmarks_upd('Release Note 1.11 Extra', 2, now());
 
-CALL sp_bookmarks_del(2);
 
 CALL sp_contentfiles_ins(1, 'img/Notes2/22015.png', now(), now());
 
 CALL `sp_lookupuserroles_ins`(6, 'Admin App', 'have an eye for detail', now(), now()) ;
 
 CALL `sp_users_upd`(1, 'Nisha', 'Izzati', 'nisha@petronas.com', now(), now());
-
-CALL `sp_lookupuserroles_del`(6);
 
 CALL `sp_lookupuserroles_upd`(5, 'User Admin', 'User Admin', now());
 
@@ -2170,7 +2157,6 @@ CALL `sp_searchterm_sel`();
 
 CALL sp_faq_upd(1, 'What is a release note?','Release notes are documents that are distributed with software products', true, true, now());
   
-CALL sp_faq_del(2);
 
 CALL `sp_feedback_upd`(1,'Feedback upd', 4, now());
 

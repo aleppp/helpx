a582fc5e68a39e850091d9330e2330edae371cc0
Fatin Nurliana Zainal (DE-SWE/DIGITAL) <fatinnurliana.zainal@PETRONAS.COM>
2021-12-30 12:01:38 +0800

    Added display data table for Feedbacklist under release note

--- Application/src/components/Feedback/FeedbackListCCRN/index.js
+++ Application/src/components/Feedback/FeedbackListCCRN/index.js
@@ -1,43 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./style.css";
 import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function FeedbackListCCRN() {
 
-  const FeedbackData = [
-    {
-      UserName: "Anna Park",
-      Rating: <img src={process.env.PUBLIC_URL + "/images/rate_5.png"}/>,
-      Feedback: "The release note is so helpful",
-      Read: "Mark as Read",
-      Date: '01/12/2021'
-    },
-
-    {
-      UserName: "Sarah binti Abdullah",
-      Rating: <img src={process.env.PUBLIC_URL + "/images/rate_3.png"}/>,
-      Feedback: "I don't really understand about this release note. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      Read: "Mark as Unread",
-      Date: '09/03/2021'
-    },
-
-    {
-      UserName: "Ali",
-      Rating: <img src={process.env.PUBLIC_URL + "/images/rate_1.png"}/>,
-      Feedback: "The release note is too complicated to understand!",
-      Read: "Mark as Read",
-      Date: '18/05/2021'
-    },
-
-    {
-      UserName: "Aaron",
-      Rating: <img src={process.env.PUBLIC_URL + "/images/rate_2.png"}/>,
-      Feedback: "So many things to read.",
-      Read: "Mark as Read",
-      Date: '30/10/2021'
-    }
-
-  ];
+  const [FeedbackCCRNList, setFeedbackCCRNList] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/feedbackccrn/sel")
+      .then((res) => {
+        if (res.status === 200) setFeedbackCCRNList(res.data[0]);
+      })
+      .catch((err) => console.log(err));
+  }, []);
   
 
   return (
@@ -54,22 +31,21 @@ function FeedbackListCCRN() {
 
         </div>
 
-        {FeedbackData.map(FeedbackData => {
-            return(
+        {FeedbackCCRNList.map((fb,i) => {
         <div class = 'feedbackList'>
         
           <><table>
             <tbody>
-                <tr>
+                <tr key={i}>
 
-                  <th>{FeedbackData.Rating}</th>
+                  <th>{fb.Rating}</th>
                   <div class="HeaderName">
-                    <th>{FeedbackData.UserName}</th>
+                    <th>{fb.UserName}</th>
                   </div>
                 </tr>
 
                 <tr>
-                  <td colSpan="2">{FeedbackData.Feedback}</td>
+                  <td colSpan="2">{fb.Feedback}</td>
                 </tr>
                 </tbody>
               </table>
@@ -77,16 +53,14 @@ function FeedbackListCCRN() {
                   <table>
                     <tbody>
                     <tr>
-                      <td class = "readdata"><button>{FeedbackData.Read}</button></td>
-                      <td><p>{FeedbackData.Date}</p></td>
+                      <td class = "readdata"><button>{fb.Read}</button></td>
+                      <td><p>{fb.Date}</p></td>
                       <td><img src={process.env.PUBLIC_URL + "/images/read_icon.png"}></img></td>
                     </tr>
                     </tbody>
                   </table>
                 </div></>
-           
         </div> 
-        );
           }
         )
         }      

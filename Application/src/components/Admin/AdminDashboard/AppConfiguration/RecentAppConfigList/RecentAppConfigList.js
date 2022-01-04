15e49eb59d8c3727ae1749d84ecb0f970553aadc
Alif Muqri Hazmi <alifmuqri.hazmi@petronas.com>
2022-01-04 23:41:18 +0800

    display data from db on recent applications configurations under admin dashboard

--- Application/src/components/Admin/AdminDashboard/AppConfiguration/RecentAppConfigList/RecentAppConfigList.js
+++ Application/src/components/Admin/AdminDashboard/AppConfiguration/RecentAppConfigList/RecentAppConfigList.js
@@ -1,26 +1,38 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "../style.css";
 
 function RecentAppConfigList() {
-  const data = [
-    {
-      date: "Date & Time",
-      appName: "Application Name",
-      changes: "Changes",
-      user: "User",
-    },
-    {
-      date: "1/11/2021 9:00PM",
-      appName: "AlphaOil",
-      changes: "Background Color Changed",
-      user: "Admin 1",
-    },
-    {
-      date: "3/11/2021 2:30PM",
-      appName: "Petronas Up",
-      changes: "URL Defined",
-      user: "Admin 2",
-    },
-  ];
+  const [recentApp, setRecentApp] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/api/configuredapps/2")
+      .then((res) => {
+        if (res.status === 200) setRecentApp(res.data[0]);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  // const data = [
+  //   {
+  //     date: "Date & Time",
+  //     appName: "Application Name",
+  //     changes: "Changes",
+  //     user: "User",
+  //   },
+  //   {
+  //     date: "1/11/2021 9:00PM",
+  //     appName: "AlphaOil",
+  //     changes: "Background Color Changed",
+  //     user: "Admin 1",
+  //   },
+  //   {
+  //     date: "3/11/2021 2:30PM",
+  //     appName: "Petronas Up",
+  //     changes: "URL Defined",
+  //     user: "Admin 2",
+  //   },
+  // ];
   return (
     <div className="RecentAppConfigList" data-testid="divShows">
       <h2>
@@ -29,24 +41,22 @@ function RecentAppConfigList() {
       </h2>
       <div>
         <table id="recent-app">
-          <tr>
-            <th>{data[0].date}</th>
-            <th>{data[0].appName}</th>
-            <th>{data[0].changes}</th>
-            <th>{data[0].user}</th>
-          </tr>
-          <tr>
-            <td>{data[1].date}</td>
-            <td>{data[1].appName}</td>
-            <td>{data[1].changes}</td>
-            <td>{data[1].user}</td>
-          </tr>
-          <tr>
-            <td>{data[2].date}</td>
-            <td>{data[2].appName}</td>
-            <td>{data[2].changes}</td>
-            <td>{data[2].user}</td>
-          </tr>
+          <thead>
+            <th>Date & Time</th>
+            <th>Application Name</th>
+            <th>Changes</th>
+            <th>User</th>
+          </thead>
+          <tbody>
+            {recentApp.map((app, i) => (
+              <tr key={i}>
+                <td>{app.Datetime}</td>
+                <td>{app.ApplicationName}</td>
+                <td width="500px">{app.Changes}</td>
+                <td>{app.User}</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     </div>

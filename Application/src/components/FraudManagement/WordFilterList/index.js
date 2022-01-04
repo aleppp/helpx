0938c015187496a4faf1a587a2d90a0914587fb6
Nurul Syahirah Binti Mokhtar <nurulsyahirah.mokhtar@petronas.com>
2022-01-04 16:46:40 +0800

    Added db hook for display word filter list

--- Application/src/components/FraudManagement/WordFilterList/index.js
+++ Application/src/components/FraudManagement/WordFilterList/index.js
@@ -1,62 +1,69 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./style.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "../../Buttons/Buttons";
 
 export default function WordFilterList() {
+  const [FraudCCList, setFraudCCList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/fraudmanagement/sel")
+      .then((res) => {
+        if (res.status === 200) setFraudCCList(res.data[0]);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const button = [
+    {
+      type: "button-blue",
+      text: "Add New",
+    },
+  ];
+
   return (
     <>
-      <div className="container">
-        <h1>Word Filter List</h1>
-        <table className="table table-borderless text-center">
+      <div className="overall">
+        <table className="table table-borderless">
           <thead>
-            <tr>
-              <td colSpan={2}></td>
+            <tr className="tdashboard">
+              <td colSpan={2}>
+                <div className="title">Word Filter List</div>
+                <div className="desc">
+                  <p>
+                    {" "}
+                    Words you specify below will be filtered from all user input
+                    (e.g. release notes, documentation and FAQ). All filtered
+                    words will be highlighted during creation of new content and
+                    cannot be submit for approval until the highlighted words
+                    are removed.
+                  </p>
+                </div>
+              </td>
             </tr>
             <tr>
               <th>ID</th>
               <th>Term</th>
             </tr>
           </thead>
-
           <tbody>
-            {data.map((item) => {
-              return (
-                <tr key={item.ID}>
-                  <td>{item.ID}</td>
-                  <td>{item.Term}</td>
-                </tr>
-              );
-            })}
+            {FraudCCList.map((fraudcc, i) => (
+              <tr key={i}>
+                <td>{fraudcc.id}</td>
+                <td>{fraudcc.term}</td>
+              </tr>
+            ))}
           </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan={2}>
+                <p className="foot"></p>
+              </td>
+            </tr>
+          </tfoot>
         </table>
       </div>
     </>
   );
 }
-
-const data = [
-  {
-    ID: "WFL1",
-    Term: "Bodo",
-  },
-
-  {
-    ID: "WFL2",
-    Term: "Bodoh",
-  },
-
-  {
-    ID: "WFL3",
-    Term: "Bodohhhhh",
-  },
-
-  {
-    ID: "WFL4",
-    Term: "Bodoooooo",
-  },
-
-  {
-    ID: "WFL5",
-    Term: "Bodohh",
-  },
-];
-

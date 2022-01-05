a0dab6febc07bb5d9331633b854ccb82e420e59a
Alif Muqri Hazmi <alifmuqri.hazmi@petronas.com>
2022-01-05 16:56:33 +0800

    Add useeffect to display data on component Pending Application Configurations under admin dashboard

--- Application/src/components/Admin/AdminDashboard/AppConfiguration/PendingAppConfigList/index.js
+++ Application/src/components/Admin/AdminDashboard/AppConfiguration/PendingAppConfigList/index.js
@@ -1,47 +1,54 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "../style.css";
 import Button from "../../../../Buttons/Buttons";
 
-const button = [
-    {
-        type: "button-blue-small",
-        text: "Configure",
-    },
-];
 function PendingAppConfigList() {
-    return (
-        <div className="PendingAppConfigList">
-            <h2>
-                Pending Configuration <br />
-                Application
-            </h2>
-            <div>
-                <table>
-                    <tr>
-                        <th>Application</th>
-                        <th>Description</th>
-                        <th>Action</th>
+  const [unconfiguredApps, setUnconfiguredApps] = useState([]);
 
-                    </tr>
-                    <tr>
-                        <td>Alpha Oil</td>
-                        <td>Stylesheet not defined!</td>
-                        <td>
-                            <Button button={button[0]}></Button>
-                        </td>
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/api/configuredapps/1")
+      .then((res) => {
+        if (res.status === 200) setUnconfiguredApps(res.data[0]);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-                    </tr>
-                    <br />
-                    <tr>
-                        <td>Petronas Up</td>
-                        <td>URL not defined!</td>
-                        <td>
-                            <Button button={button[0]}></Button>
-                        </td>
-                    </tr>
-                </table>
-            </div>
-        </div>
-    )
+  const button = [
+    {
+      type: "button-blue-small",
+      text: "Configure",
+    },
+  ];
+  return (
+    <div className="PendingAppConfigList">
+      <h2>
+        Pending Configuration <br />
+        Application
+      </h2>
+      <div>
+        <table id="pending-app">
+          <thead>
+            <th>Application</th>
+            <th>Description</th>
+            <th>Action</th>
+          </thead>
+          <tbody>
+            {unconfiguredApps.map((unconfigured, i) => (
+              <tr key={i}>
+                <td>{unconfigured.Application}</td>
+                <td>{unconfigured.Description}</td>
+                <td>
+                  <Button button={button[0]}></Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default PendingAppConfigList;

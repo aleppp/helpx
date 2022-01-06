0309b465c9422f0caad925d7c7ebf8706122955f
Nor Haizan Nor Azman <norhaizan.norazman@petronas.com>
2022-01-06 16:41:28 +0800

    Fixed stylesheet by removing inline style and all style has been moved inside external stylesheet

--- Application/src/components/Bookmark/BookmarkListEU/index.js
+++ Application/src/components/Bookmark/BookmarkListEU/index.js
@@ -55,70 +55,58 @@ export default function BookmarkListEU() {
       </div>
       <div className="BookmarkList-EU">
         <div className="overall">
-          <div
-            id="container"
-            style={{
-              margin: `30px`,
-            }}
-          >
-            <h1>
-              <b>{"Bookmark"}</b>
-            </h1>
-            <table
-              className="table table-borderless text-center"
-              style={{
-                border: `1px solid black`,
-              }}
-            >
-              <thead>
-                <tr>
-                  <td colSpan={4}></td>
-                </tr>
+          <h1>
+            <b>{"Bookmark"}</b>
+          </h1>
+          <table className="table table-borderless text-center">
+            <thead>
+              <tr>
+                <td colSpan={4}></td>
+              </tr>
 
-                <tr style={{}}>
-                  <th>Date</th>
-                  <th>Title</th>
-                  <th>Link (url)</th>
-                  <th>Action</th>
-                </tr>
-              </thead>
+              <tr style={{}}>
+                <th>Date</th>
+                <th>Title</th>
+                <th>Link (url)</th>
+                <th>Action</th>
+              </tr>
+            </thead>
 
-              <tbody>
-                {BookmarkEUList.map((bm, i) => (
-                  <tr key={i}>
-                    <td>{bm.DateCreated}</td>
-                    <td>
-                      <TextField
-                        id="outlined-basic"
-                        defaultValue={bm.BookmarkName}
-                        variant="outlined"
-                      />
-                    </td>
-                    <td>
-                      <a href={bm.URL}>{bm.URL}</a>
-                    </td>
-                    <td>
-                      <button1 type="button" class="btn btn-info">
-                        Save
-                      </button1>{" "}
-                      <button2 type="button" class="btn btn-danger">
-                        Delete
-                      </button2>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-            <Pagination
-              className="pageBar"
-              count={count}
-              size="large"
-              color="primary"
-              page={page}
-              shape="rounded"
-              onChange={handleChange}
-            />
-          </div>
+            <tbody>
+              {BookmarkEUList.map((bm, i) => (
+                <tr key={i}>
+                  <td>{bm.DateCreated}</td>
+                  <td>
+                    <TextField
+                      id="outlined-basic"
+                      defaultValue={bm.BookmarkName}
+                      variant="outlined"
+                    />
+                  </td>
+                  <td>
+                    <a href={bm.URL}>{bm.URL}</a>
+                  </td>
+                  <td>
+                    <button1 type="button" class="btn btn-info">
+                      Save
+                    </button1>{" "}
+                    <button2 type="button" class="btn btn-danger">
+                      Delete
+                    </button2>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <Pagination
+            className="pageBar"
+            count={count}
+            size="large"
+            color="primary"
+            page={page}
+            shape="rounded"
+            onChange={handleChange}
+          />
         </div>
       </div>
     </>

52c425491ffdda6566fffd4bb4c83a8463c50c82
Toba <mhazmannasrullah.suk@petronas.com>
2022-01-07 07:15:55 +0800

    Add function to display users panel on admin dashboard

---
+++ Application/src/components/Admin/AdminDashboard/UsersPanel/UsersPanel.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+import "./UsersPanel.css";
+
+function UsersPanel() {
+  return (
+    <div className="user-panel">
+      <h2>Users Role</h2>
+      <div class="green-circle"></div>
+      <span>User123</span>
+      <br />
+      <div class="green-circle"></div>
+      <span>User312</span>
+      <br />
+      <br />
+      <h2>Not active user</h2>
+      <div class="gray-circle"></div>
+      <span>User216</span>
+    </div>
+  );
+}
+
+export default UsersPanel;

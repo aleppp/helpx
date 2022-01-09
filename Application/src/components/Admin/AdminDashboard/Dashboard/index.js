e8b0a577ad2fd9626496b4ff82aca4e958b2e941
Balqis Batrisyia Mohd Nizam <balqisbatrisyia.mniz@petronas.com>
2022-01-09 22:49:37 +0800

    Fix the code for index.js under Dashboard according to the suggestion

--- Application/src/components/Admin/AdminDashboard/Dashboard/index.js
+++ Application/src/components/Admin/AdminDashboard/Dashboard/index.js
@@ -9,6 +9,12 @@ function Dashboard() {
 
   useEffect(() => {
     axios
+      .get("http://localhost:8080/countUser/sel")
+      .then((res) => {
+        if (res.status === 200) setCountUser(res.data[0]);
+      })
+      .catch((err) => console.log(err));
+    axios
       .get("http://localhost:8080/integratedapps/sel")
       .then((res) => {
         if (res.status === 200) setCountApp(res.data[0]);
@@ -16,16 +22,7 @@ function Dashboard() {
       .catch((err) => console.log(err));
   }, []);
 
-  useEffect(() => {
-    axios
-      .get("http://localhost:8080/countUser/sel")
-      .then((res) => {
-        if (res.status === 200) setCountUser(res.data[0]);
-      })
-      .catch((err) => console.log(err));
-  }, []);
-
-  const counter = [
+  const arrayTitle = [
     { title: "Active Users" },
     { title: "Number of Integrated Application" },
   ];
@@ -34,13 +31,13 @@ function Dashboard() {
     <div className="dashboard">
       <div className="ActiveUser">
         <CounterWidget
-          title={counter[0].title}
+          title={arrayTitle[0].title}
           count={countUser.map((user) => user.ActiveUser)}
         />
       </div>
       <div className="IntegratedApp">
         <CounterWidget
-          title={counter[1].title}
+          title={arrayTitle[1].title}
           count={countApp.map((app) => app.IntegratedApp)}
         />
       </div>

fb9561b813c853ff82b4d49dd79dd7b6aeaa27d2
Alif Muqri Hazmi <alifmuqri.hazmi@petronas.com>
2022-01-17 00:05:59 +0800

    format pending app config list and recent configured app to be responsive

--- Application/src/components/Admin/AdminDashboard/AppConfiguration/PendingAppConfigList/index.js
+++ Application/src/components/Admin/AdminDashboard/AppConfiguration/PendingAppConfigList/index.js
@@ -22,30 +22,32 @@ function PendingAppConfigList() {
     },
   ];
   return (
-    <div className="PendingAppConfigList">
-      <h2>
-        Pending Configuration <br />
-        Application
-      </h2>
-      <div>
-        <table id="pending-app">
-          <thead>
-            <th>Application</th>
-            <th>Description</th>
-            <th>Action</th>
-          </thead>
-          <tbody>
-            {unconfiguredApps.map((unconfigured, i) => (
-              <tr key={i}>
-                <td>{unconfigured.Application}</td>
-                <td>{unconfigured.Description}</td>
-                <td>
-                  <Button button={button[0]}></Button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+    <div className="container-lg m-3">
+      <div id="PendingAppConfigList" className="d-flex flex-column">
+        <h2>
+          Pending Configuration <br />
+          Application
+        </h2>
+        <div>
+          <table id="pending-app">
+            <thead>
+              <th>Application</th>
+              <th>Description</th>
+              <th>Action</th>
+            </thead>
+            <tbody>
+              {unconfiguredApps.slice(0, 3).map((unconfigured, i) => (
+                <tr key={i}>
+                  <td>{unconfigured.Application}</td>
+                  <td>{unconfigured.Description}</td>
+                  <td>
+                    <Button button={button[0]}></Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );

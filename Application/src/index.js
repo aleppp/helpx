9de7b0a0529770790a714f8d210713c6a1857092
Alif Muqri Hazmi <alifmuqri.hazmi@petronas.com>
2022-01-25 16:16:03 +0800

    Update: refactor counter widget that use for active users and number of applications to be responsive

--- Application/src/index.js
+++ Application/src/index.js
@@ -8,11 +8,9 @@ import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
-  <>
-    <Router>
-      <Route component={App} />
-    </Router>
-  </>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById("root")
 );
 

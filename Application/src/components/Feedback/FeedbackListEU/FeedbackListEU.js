d217785a7b99349eea35e47dcb2ce28d2f3809a0
Nurul Syahirah Binti Mokhtar <nurulsyahirah.mokhtar@petronas.com>
2022-01-27 23:02:32 +0800

    Fixed stylesheets for feedback components

--- Application/src/components/Feedback/FeedbackListEU/FeedbackListEU.js
+++ Application/src/components/Feedback/FeedbackListEU/FeedbackListEU.js
@@ -56,137 +56,134 @@ export default function FeedbackEUList() {
 
   return (
     <>
-      <div className="container-fluid m-0 g-0">
-        <div className="d-inline flex">
-          <div className="row flex-wrap">
-            <div className="col-auto">
-              <div className="FeedbackList-EU">
-                <table className="table table-borderless">
-                  <thead>
-                    <tr className="tdashboard">
-                      <td colSpan={5}>
-                        <div className="title">Feedback List</div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <th>Content</th>
-                      <th>Date</th>
-                      <th>Rating</th>
-                      <th>Feedback</th>
-                      <th>Action</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {FeedbackDataEU.currentData().map((fb, index) => {
-                      return (
-                        <tr key={index}>
-                          <td>
-                            <a href={fb.title}>{fb.title}</a>
-                          </td>
-                          <td>{fb.DateCreated}</td>
-                          <td>
-                            <div class="dropdown">
-                              {(function () {
-                                if (fb.Rating == "1") {
-                                  return (
-                                    <img
-                                      src={
-                                        process.env.PUBLIC_URL +
-                                        "/images/rate_1.png"
-                                      }
-                                      alt="Very Dissatisfied"
-                                    />
-                                  );
-                                } else if (fb.Rating == "2") {
-                                  return (
-                                    <img
-                                      src={
-                                        process.env.PUBLIC_URL +
-                                        "/images/rate_2.png"
-                                      }
-                                      alt="Very Dissatisfied"
-                                    />
-                                  );
-                                } else if (fb.Rating == "3") {
-                                  return (
-                                    <img
-                                      src={
-                                        process.env.PUBLIC_URL +
-                                        "/images/rate_3.png"
-                                      }
-                                      alt="Neutral"
-                                    />
-                                  );
-                                } else if (fb.Rating == "4") {
-                                  return (
-                                    <img
-                                      src={
-                                        process.env.PUBLIC_URL +
-                                        "/images/rate_4.png"
-                                      }
-                                      alt="Satisfied"
-                                    />
-                                  );
-                                } else {
-                                  return (
-                                    <img
-                                      src={
-                                        process.env.PUBLIC_URL +
-                                        "/images/rate_5.png"
-                                      }
-                                      alt="Very Satisfied"
-                                    />
-                                  );
-                                }
-                              })()}
-                              <SelectRating />
-                            </div>
-                          </td>
+      <div className="container-fluid" id="FeedbackListEU">
+        <div className="row g-0 d-flex">
+          <div className="d-line flex d-sm-flex-column">
+            <div className="col-lg-12">
+              <table>
+                <thead>
+                  <tr className="tdashboard">
+                    <td colSpan={5}>
+                      <div className="title">Feedback List</div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>Content</th>
+                    <th>Date</th>
+                    <th>Rating</th>
+                    <th>Feedback</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {FeedbackDataEU.currentData().map((fb, index) => {
+                    return (
+                      <tr key={index}>
+                        <td>
+                          <a href={fb.title}>{fb.title}</a>
+                        </td>
+                        <td>{fb.DateCreated}</td>
+                        <td>
+                          <div class="dropdown">
+                            {(function () {
+                              if (fb.Rating == "1") {
+                                return (
+                                  <img
+                                    src={
+                                      process.env.PUBLIC_URL +
+                                      "/images/rate_1.png"
+                                    }
+                                    alt="Very Dissatisfied"
+                                  />
+                                );
+                              } else if (fb.Rating == "2") {
+                                return (
+                                  <img
+                                    src={
+                                      process.env.PUBLIC_URL +
+                                      "/images/rate_2.png"
+                                    }
+                                    alt="Very Dissatisfied"
+                                  />
+                                );
+                              } else if (fb.Rating == "3") {
+                                return (
+                                  <img
+                                    src={
+                                      process.env.PUBLIC_URL +
+                                      "/images/rate_3.png"
+                                    }
+                                    alt="Neutral"
+                                  />
+                                );
+                              } else if (fb.Rating == "4") {
+                                return (
+                                  <img
+                                    src={
+                                      process.env.PUBLIC_URL +
+                                      "/images/rate_4.png"
+                                    }
+                                    alt="Satisfied"
+                                  />
+                                );
+                              } else {
+                                return (
+                                  <img
+                                    src={
+                                      process.env.PUBLIC_URL +
+                                      "/images/rate_5.png"
+                                    }
+                                    alt="Very Satisfied"
+                                  />
+                                );
+                              }
+                            })()}
+                            <SelectRating />
+                          </div>
+                        </td>
 
-                          <td>
-                            <TextField
-                              id="outlined-normal"
-                              variant="outlined"
-                              label=" "
-                              multiline
-                              maxRows={4}
-                              defaultValue={fb.Feedback}
-                            >
-                              {fb.Feedback}
-                            </TextField>
-                          </td>
-                          <td>
-                            <Button button={button[0]}></Button>
-                          </td>
-                        </tr>
-                      );
-                    })}
-                  </tbody>
-                  <tfoot>
-                    <tr>
-                      <td colSpan={5}>
-                        <p className="foot"></p>
-                      </td>
-                    </tr>
-                  </tfoot>
-                </table>
-                <Pagination
-                  className="pageBar"
-                  count={count}
-                  size="large"
-                  color="primary"
-                  page={page}
-                  shape="rounded"
-                  onChange={handleChange}
-                />
-              </div>
+                        <td>
+                          <TextField
+                            id="outlined-normal"
+                            variant="outlined"
+                            label=" "
+                            multiline
+                            maxRows={4}
+                            defaultValue={fb.Feedback}
+                          >
+                            {fb.Feedback}
+                          </TextField>
+                        </td>
+                        <td>
+                          <Button button={button[0]}></Button>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+                <tfoot>
+                  <tr>
+                    <td colSpan={5}>
+                      <p className="foot"></p>
+                    </td>
+                  </tr>
+                </tfoot>
+              </table>
+              <Pagination
+                className="pageBar"
+                count={count}
+                size="large"
+                color="primary"
+                page={page}
+                shape="rounded"
+                onChange={handleChange}
+              />
             </div>
           </div>
         </div>
       </div>
+      {/* </div> */}
+      {/* </div> */}
     </>
   );
-}
-
-{
 }

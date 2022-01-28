18e95309425e055ecd08b6da178d54bc80ed015f
Balqis Batrisyia Mohd Nizam <balqisbatrisyia.mniz@petronas.com>
2022-01-28 16:16:48 +0800

    Fix the const for delete button

--- Application/src/components/Admin/AdminManagement/RolesManagement/index.js
+++ Application/src/components/Admin/AdminManagement/RolesManagement/index.js
@@ -8,7 +8,7 @@ import { DeleteRole } from "./RolesManagementBtn/DeleteRole";
 function RolesManagement() {
   const [RolesManagement, setRolesManagement] = useState([]);
   const [showEdit, setShowEdit] = useState(false);
-  const [showDel, setShowDel] = useState(true);
+  const [showDel, setShowDel] = useState(false);
 
   useEffect(() => {
     axios

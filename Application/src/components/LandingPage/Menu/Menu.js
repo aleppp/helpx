74864d5e00a68ad7ad2321e8742f4a4744fc1586
Alif Muqri Hazmi <alifmuqri.hazmi@petronas.com>
2022-01-31 11:51:39 +0800

    refactor body content for landing page using grid

--- Application/src/components/LandingPage/Menu/Menu.js
+++ Application/src/components/LandingPage/Menu/Menu.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Menu.css";
-import Buttons from "../../Buttons/Buttons";
+import "../../Buttons/Buttons.css";
 
 const Button = [
   {
@@ -15,22 +15,33 @@ const Button = [
 
 function Menu() {
   return (
-    <div>
+    <div className="container-fluid g-0">
       <div className="content">
-        <div className="content-1">
-          <img
-            className="images-1"
-            src={process.env.PUBLIC_URL + "/images/doc.png"}
-          />
-          <Buttons button={Button[0]}></Buttons>
-        </div>
-        <div className="content-2">
-          {" "}
-          <img
-            className="images-1"
-            src={process.env.PUBLIC_URL + "/images/note.png"}
-          />
-          <Buttons button={Button[1]}></Buttons>
+        <div className="row">
+          <div className="col">
+            <div className="row">
+              <div className="content-1">
+                <img
+                  className="images-1"
+                  src={process.env.PUBLIC_URL + "/images/doc.png"}
+                  alt="release-notes"
+                />
+                <button className="button-green-big">Release Notes</button>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="row">
+              <div className="content-2">
+                <img
+                  className="images-1"
+                  src={process.env.PUBLIC_URL + "/images/note.png"}
+                  alt="documentation"
+                />
+                <button className="button-green-big">Documentation</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>

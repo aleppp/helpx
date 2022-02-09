54fcdde26468937278f15d54beb01e09a476ef93
Balqis Batrisyia Mohd Nizam <balqisbatrisyia.mniz@petronas.com>
2022-02-09 19:59:31 +0800

    refactor and remove the white space

--- Application/src/components/Admin/AdminManagement/UserManagement/index.js
+++ Application/src/components/Admin/AdminManagement/UserManagement/index.js
@@ -17,60 +17,64 @@ function UserManagement() {
   }, []);
 
   return (
-    <div>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="user-management-component">
-            <h1>User Management</h1>
-            <button className="button-blue">Add New</button>
+    <div className="container-fluid" id="user-mana">
+      <div className="row">
+        <div className="col-lg-12 col-md-6 col-sm-3">
+          <div className="row">
+            <h1 className="text-start">User Management</h1>
           </div>
-        </div>
+          <div className="row ">
+            <div className="">
+              <button className="button-blue">Add New</button>
+            </div>
+          </div>
 
-        <div>
-          <table>
-            <thead>
-              <th>
-                <div className="img-expandMore">
-                  User ID
+          <div className="row">
+            <table>
+              <thead>
+                <th>
+                  <div className="img-expandMore">
+                    User ID
+                    <img
+                      className="dropdown"
+                      src={process.env.PUBLIC_URL + "/images/expandMore.png"}
+                    />
+                  </div>
+                </th>
+                <th>
+                  User Name{" "}
                   <img
                     className="dropdown"
                     src={process.env.PUBLIC_URL + "/images/expandMore.png"}
                   />
-                </div>
-              </th>
-              <th>
-                User Name{" "}
-                <img
-                  className="dropdown"
-                  src={process.env.PUBLIC_URL + "/images/expandMore.png"}
-                />
-              </th>
-              <th>
-                User Email{" "}
-                <img
-                  className="dropdown"
-                  src={process.env.PUBLIC_URL + "/images/expandMore.png"}
-                />
-              </th>
-              <th>
-                Application{" "}
-                <img
-                  className="dropdown"
-                  src={process.env.PUBLIC_URL + "/images/expandMore.png"}
-                />
-              </th>
-              <th> Content Contributor </th>
-              <th> Content Approver </th>
-              <th> System Admin </th>
-              <th> Action </th>
-              <th></th>
-            </thead>
-            <tbody>
-              {UserManagement.map((userDelete, i) => (
-                <DeleteButton userDelete={userDelete} i={i} />
-              ))}
-            </tbody>
-          </table>
+                </th>
+                <th>
+                  User Email{" "}
+                  <img
+                    className="dropdown"
+                    src={process.env.PUBLIC_URL + "/images/expandMore.png"}
+                  />
+                </th>
+                <th>
+                  Application{" "}
+                  <img
+                    className="dropdown"
+                    src={process.env.PUBLIC_URL + "/images/expandMore.png"}
+                  />
+                </th>
+                <th> Content Contributor </th>
+                <th> Content Approver </th>
+                <th> System Admin </th>
+                <th> Action </th>
+                <th></th>
+              </thead>
+              <tbody>
+                {UserManagement.map((userDelete, i) => (
+                  <DeleteButton userDelete={userDelete} i={i} />
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>

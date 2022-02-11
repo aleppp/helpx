a874c72adf6ea5bab1f7c6eb04c0a7b621935a79
Noreliza Khusari <noreliza.khusari@petronas.com>
2022-02-11 15:19:13 +0800

    Added sorting function on fraud admin

--- Application/src/components/FraudManagement/FraudAdmin/index.js
+++ Application/src/components/FraudManagement/FraudAdmin/index.js
@@ -1,14 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer, useMemo } from "react";
 import axios from "axios";
 import "./style.css";
-import Pagination from "../../Layout/Navigation/Pagination/pagination";
+import { Pagination } from "@mui/material";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { EditFraud } from "./FraudBtn";
-import { AddFraud } from "./FraudBtn/addnew";
-import { DeleteFraud } from "./FraudBtn/delete";
 
-function FraudConfig() {
+export default function FraudConfig() {
   const [fraudManagement, setFraudManagement] = useState([]);
+  const checkfilter = fraudManagement;
+
+  //for sorting
+  const [currentCreated, setCurrentCreated] = useState("createdUnsort");
+  const [dateClicked, setDateClicked] = useState([]);
+
   const [showEdit, setShowEdit] = useState(false);
   const [showDel, setShowDel] = useState(false);
   const [showAdd, setShowAdd] = useState(false);
@@ -22,10 +26,115 @@ function FraudConfig() {
       .catch((err) => console.log(err));
   }, []);
 
+  //for pagination
+  let [page, setPage] = useState(1);
+  const PER_PAGE = 4;
+
+  const _DATA = usePagination(checkfilter, PER_PAGE);
+  const count = Math.ceil(checkfilter.length / PER_PAGE);
+
+  //handler for MUI oagination
+  const handleChange = (e, p) => {
+    setPage(p);
+    _DATA.jump(p);
+  };
+
+  const initialState = {
+    isCreatedSorted: false,
+    isCreatedDesc: false,
+  };
+
+  const [state, dispatch] = useReducer(sortReducer, initialState);
+
+  function sortReducer(state, action) {
+    if (dateClicked === "term" || dateClicked === "id") {
+      switch (action.type) {
+        case "createdUnsort":
+          return {
+            isCreatedSorted: true,
+            isCreatedDesc: false,
+          };
+        case "createdAsc":
+          return {
+            isCreatedSorted: true,
+            isCreatedDesc: true,
+          };
+        case "createdDesc":
+          return {
+            isCreatedSorted: true,
+            isCreatedDesc: false,
+          };
+        default:
+          return {
+            isCreatedSorted: state.isCreatedSorted,
+            isCreatedDesc: state.isCreatedDesc,
+          };
+      }
+    }
+  }
+
+  function dispatchSort(e) {
+    let sortData = [...fraudManagement];
+    setDateClicked(e.currentTarget.id);
+    let targetDate = e.currentTarget.id;
+
+    if (targetDate === "id") {
+      if (currentCreated === "createdUnsort") {
+        setCurrentCreated("createdAsc");
+        dispatch({ type: "createdUnsort" });
+        sortData = sortData.sort((a, b) => a.id - b.id);
+      } else if (currentCreated === "createdAsc") {
+        setCurrentCreated("createdDesc");
+        dispatch({ type: "createdAsc" });
+        sortData = sortData.sort((a, b) => b.id - a.id);
+      } else {
+        setCurrentCreated("createdAsc");
+        dispatch({ type: "createdUnsort" });
+        sortData = sortData.sort((a, b) => a.id - b.id);
+      }
+    }
+
+    if (targetDate === "term") {
+      if (currentCreated === "createdUnsort") {
+        setCurrentCreated("createdAsc");
+        dispatch({ type: "createdUnsort" });
+        sortData = sortData.sort((a, b) => a.term.localeCompare(b.term));
+      } else if (currentCreated === "createdAsc") {
+        setCurrentCreated("createdDesc");
+        dispatch({ type: "createdAsc" });
+        sortData = sortData.sort((a, b) => b.term.localeCompare(a.term));
+      } else {
+        setCurrentCreated("createdAsc");
+        dispatch({ type: "createdUnsort" });
+        sortData = sortData.sort((a, b) => a.term.localeCompare(b.term));
+      }
+    }
+    setFraudManagement(sortData);
+  }
+
+  function usePagination(data, itemsPerPage) {
+    const [currentPage, setCurrentPage] = useState(1);
+    const maxPage = Math.ceil(data.length / itemsPerPage);
+
+    // Specify which data will be displayed in the table
+    function currentData() {
+      const begin = (currentPage - 1) * itemsPerPage;
+      const end = begin + itemsPerPage;
+
+      return data.slice(begin, end);
+    }
+
+    function jump(page) {
+      const pageNumber = Math.max(1, page);
+      setCurrentPage((currentPage) => Math.min(pageNumber, maxPage));
+    }
+
+    return { jump, currentData, currentPage, maxPage };
+  }
   return (
     <div className="container-fluid">
       <div className="row">
-        <div className="fraud-config-component">
+        <div className="fraud-management-component">
           <h1 className="fraud-title">Fraud Management</h1>
           <button className="button-blue" onClick={() => setShowAdd(!showAdd)}>
             Add New
@@ -34,22 +143,35 @@ function FraudConfig() {
       </div>
       <table id="fraud">
         <tr>
-          <th>
-            <div className="dropdown dropdown-admin">
-              <p className="dropdown-toggle" data-bs-toggle="dropdown">
-                ID
-              </p>
-              <div className="dropdown-menu dropdown-m-admin">
-                <input id="id" type="text" />
-              </div>
-            </div>
+          <th id="id" onClick={(e) => dispatchSort(e)}>
+            ID
+            {state.isCreatedSorted ? (
+              state.isCreatedDesc ? (
+                <img src={process.env.PUBLIC_URL + "/icons/descend.svg"} />
+              ) : (
+                <img src={process.env.PUBLIC_URL + "/icons/ascend.svg"} />
+              )
+            ) : (
+              <img src={process.env.PUBLIC_URL + "/icons/unsort.svg"} />
+            )}
           </th>
-          <th>Term</th>
+          <th id="term" onClick={(e) => dispatchSort(e)}>
+            Term
+            {state.isCreatedSorted ? (
+              state.isCreatedDesc ? (
+                <img src={process.env.PUBLIC_URL + "/icons/descend.svg"} />
+              ) : (
+                <img src={process.env.PUBLIC_URL + "/icons/ascend.svg"} />
+              )
+            ) : (
+              <img src={process.env.PUBLIC_URL + "/icons/unsort.svg"} />
+            )}
+          </th>
           <th>Action</th>
         </tr>
 
         <tbody>
-          {fraudManagement.map((fraud, index) => (
+          {_DATA.currentData().map((fraud, index) => (
             <tr key={index}>
               <td>{fraud.id}</td>
               <td>{fraud.term}</td>
@@ -68,19 +190,9 @@ function FraudConfig() {
                 </button>
               </td>
               <td>
-                {showAdd ? (
-                  <div>
-                    <AddFraud />
-                  </div>
-                ) : null}
                 {showEdit ? (
                   <div>
                     <EditFraud />
-                  </div>
-                ) : null}
-                {showDel ? (
-                  <div>
-                    <DeleteFraud />
                   </div>
                 ) : null}
               </td>
@@ -89,10 +201,16 @@ function FraudConfig() {
         </tbody>
       </table>
       <div>
-        <Pagination />
+        <Pagination
+          className="pageBar"
+          count={count}
+          size="large"
+          color="primary"
+          page={page}
+          shape="rounded"
+          onChange={handleChange}
+        />
       </div>
     </div>
   );
 }
-
-export default FraudConfig;

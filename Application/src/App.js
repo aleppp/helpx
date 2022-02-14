d17af68cd6f5d8cc2bf1196904ad763c83d79a6b
Mohammad Amirul Luqman Shamshi <m.miruluqman@gmail.com>
2022-02-14 15:17:26 +0800

    update: add protectedroute and conditional navbar for different role

--- Application/src/App.js
+++ Application/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./components/LandingPage/LandingPage";
 import Auth from "./services/Auth/Auth";
@@ -7,9 +7,14 @@ import Callback from "./services/Auth/Callback";
 import EndUserHomePage from "./components/HomePage/EndUserHomePage/EndUserHomePage";
 import AuditLogDatatable from "./components/Admin/AdminAuditLog/AuditLogDatatable";
 import AdminHomePage from "./components/Admin/AdminDashboard";
-import NavigationBar from "./components/Layout/Navigation/AdminNavigation";
-import Dashboard from "./components/ReleaseNotes/Dashboard";
 import Content from "./components/ReleaseNotes/Content";
+import ProtectedRoute from "./services/ProtectedRoute";
+import DashboardCV from "./components/ReleaseNotes/Dashboard/DashboardCC";
+import UserNavigation from "./components/ReleaseNotes/Navigation/UserNavigation";
+import Header from "./components/LandingPage/Header/Header";
+import NavigationBar from "./components/Layout/Navigation/AdminNavigation";
+import FeedbackEUList from "./components/Feedback/FeedbackListEU/FeedbackListEU";
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -17,58 +22,64 @@ class App extends Component {
   }
 
   render() {
+    const isAdmin = localStorage.getItem("isAdmin");
     return (
       <div className="chunk">
-        <Router>
-          <NavigationBar />
-          <Switch>
-            <div className="App">
-              <Route
-                path="/"
-                exact
-                render={(props) => <LandingPage {...props} />}
-              />
-              <Route
-                path="/admin-home"
-                exact
-                render={(props) => <AdminHomePage {...props} />}
-              />
-              <Route
-                path="/callback"
-                exact
-                render={(props) => <Callback auth={this.auth} {...props} />}
-              />
-              <Route
-                path="/homepage"
-                exact
-                render={(props) => (
-                  <EndUserHomePage auth={this.auth} {...props} />
-                )}
-              />
-              <Route
-                path="/audit-logs"
-                exact
-                render={(props) => (
-                  <AuditLogDatatable auth={this.auth} {...props} />
-                )}
-              />
-              <Route
-                path="/creator-dashboard"
-                exact
-                render={(props) => (
-                  <Dashboard auth={this.auth} {...props} />
-                )}
-              />
-              <Route
-                path="/normal-editor"
-                exact
-                render={(props) => (
-                  <Content auth={this.auth} {...props} />
-                )}
-              />
-            </div>
-          </Switch>
-        </Router>
+        {(window.location.href.toString() === "http://localhost:3000/") ? null:<Header />}
+        {((window.location.href.toString() === "http://localhost:3000/") ?
+         null:(isAdmin === "true") ? <NavigationBar />:<UserNavigation/> )}
+        <Switch>
+            <Route
+              exact
+              path="/"
+              render={(props) => <LandingPage {...props} />}
+            />
+            <Route
+              exact
+              path="/callback"
+              render={(props) => <Callback auth={this.auth} {...props} />}
+            />
+            <ProtectedRoute
+              exact
+              path="/admin/home"
+              render={(props) => <AdminHomePage {...props}  />}
+            />
+            <ProtectedRoute
+              exact
+              path="/homepage"
+              render={(props) => (
+                <EndUserHomePage auth={this.auth} {...props}  />
+              )}
+            />
+            <ProtectedRoute
+              exact
+              path="/admin/audit-logs"
+              render={(props) => (
+                <AuditLogDatatable auth={this.auth} {...props}  />
+              )}
+            />
+            <ProtectedRoute
+              exact
+              path="/creator/dashboard"
+              render={(props) => (
+                <DashboardCV auth={this.auth} {...props}  />
+              )}
+            />
+            <ProtectedRoute
+              exact
+              path="/editor/normal"
+              render={(props) => (
+                <Content auth={this.auth} {...props}  />
+              )}
+            />
+            <ProtectedRoute
+              exact
+              path="/content/feedback"
+              render={(props) => (
+                <FeedbackEUList auth={this.auth} {...props}  />
+              )}
+            />
+        </Switch>
       </div>
     );
   }

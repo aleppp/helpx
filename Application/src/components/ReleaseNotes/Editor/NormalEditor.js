e2bf5a2656d2f8ae111152068f9f4083eb52e100
Nur Farahjannah Chua Jamil Chua <nurfarahjannahchua.j@petronas.com>
2022-02-16 02:07:35 +0000

    have to pass user session data and ckeditor to db

--- Application/src/components/ReleaseNotes/Editor/NormalEditor.js
+++ Application/src/components/ReleaseNotes/Editor/NormalEditor.js
@@ -1,13 +1,85 @@
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 //import SpellcheckIcon from "@mui/icons-material/Spellcheck";
 import "./NormalEditor.css";
 
-function NormalEditor() {
+/*const contentdummy = () => {
+  const [conntentdummy, setConntentdummy] = useState("");
+};
+const handleClick = () => {
+  // update the conntentdummy state property by adding a new data
+  updateContentdummy([
+    ...contentdummy,
+    {
+      userid: 1,
+      contenttypeid: 1,
+      statusid: 1,
+      isfeebackallowed: 1,
+      isvisible: 1,
+      datecreated: "...",
+      datemodified: "...",
+      datepublished: "...",
+    },
+  ]);
+};*/
+
+/*const setDraft = () => {
+  axios
+    .post("http://localhost:8080/content/ins")
+    .then((res) => {
+      if (res.status === 200) setTitle(res.data[0]);
+    })
+    .catch((err) => console.log(err));
+};*/
+
+const NormalEditor = () => {
   const clicked = () => {};
-  const [text, setText] = useState("");
+  const [body, setBody] = useState("");
   const [title, setTitle] = useState("");
+  let content = {
+    appid: [],
+    userid: [],
+    contenttypeid: [],
+    statusid: [],
+    isfeebackallowed: [],
+    isvisible: [],
+    datecreated: [],
+    datemodified: [],
+    datepublished: [],
+  };
+  const setContent = () => {
+    axios
+      .post("http://localhost:8080/content/ins", {
+        content,
+      })
+      .then((res) => {
+        if (res.status === 200) setTitle(res.data[0]);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  /*axios
+      .post("http://localhost:8080/content/ins")
+      .then((res) => {
+        if (res.status === 200) setTitle(res.data[0]);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+    this.setState({
+      userid: "1",
+      contenttypeid: "1",
+      statusid: "1",
+      isfeedbackallowed: "1",
+      isvisible: "1",
+      datecreated: "2022-02-14 00:00:00",
+      datemodified: "2022-02-14 00:00:00",
+      datepublished: "2022-02-14 00:00:00",
+    });
+  });*/
+
   return (
     <>
       <div className="NormalEditor">
@@ -20,6 +92,7 @@ function NormalEditor() {
             <input
               type="text"
               value={title}
+              onSubmit={setContent}
               onChange={(e) => setTitle(e.target.value)}
             />
           </form>
@@ -31,10 +104,18 @@ function NormalEditor() {
             <h3 className="h3">Body Content</h3>
             <CKEditor
               editor={ClassicEditor}
-              data={text}
+              data={body}
+              onSubmit={setContent}
               onChange={(event, editor) => {
                 const data = editor.getData();
-                setText(data);
+                setBody(data);
+                console.log({ event, editor, data });
+              }}
+              onBlur={(event, editor) => {
+                console.log("Blur.", editor);
+              }}
+              onFocus={(event, editor) => {
+                console.log("Focus.", editor);
               }}
             />
           </div>
@@ -42,6 +123,6 @@ function NormalEditor() {
       </div>
     </>
   );
-}
+};
 
 export default NormalEditor;

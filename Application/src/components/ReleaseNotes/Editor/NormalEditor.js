ec9e814ba9aee4cc0e76f0d6f5696b2aa28b4c7e
Nur Farahjannah Chua Jamil Chua <nurfarahjannahchua.j@petronas.com>
2022-02-17 15:26:28 +0000

    Updated the conflict code of NormalEditor.js

--- Application/src/components/ReleaseNotes/Editor/NormalEditor.js
+++ Application/src/components/ReleaseNotes/Editor/NormalEditor.js
@@ -2,37 +2,8 @@ import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-//import SpellcheckIcon from "@mui/icons-material/Spellcheck";
 import "./NormalEditor.css";
 
-/*const contentdummy = () => {
-  const [conntentdummy, setConntentdummy] = useState("");
-};
-const handleClick = () => {
-  // update the conntentdummy state property by adding a new data
-  updateContentdummy([
-    ...contentdummy,
-    {
-      userid: 1,
-      contenttypeid: 1,
-      statusid: 1,
-      isfeebackallowed: 1,
-      isvisible: 1,
-      datecreated: "...",
-      datemodified: "...",
-      datepublished: "...",
-    },
-  ]);
-};*/
-
-/*const setDraft = () => {
-  axios
-    .post("http://localhost:8080/content/ins")
-    .then((res) => {
-      if (res.status === 200) setTitle(res.data[0]);
-    })
-    .catch((err) => console.log(err));
-};*/
 const content = {
   appid: 1,
 
@@ -71,33 +42,13 @@ const NormalEditor = () => {
       .catch((err) => console.log(err));
   };
 
-  /*axios
-      .post("http://localhost:8080/content/ins")
-      .then((res) => {
-        if (res.status === 200) setTitle(res.data[0]);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-    this.setState({
-      userid: "1",
-      contenttypeid: "1",
-      statusid: "1",
-      isfeedbackallowed: "1",
-      isvisible: "1",
-      datecreated: "2022-02-14 00:00:00",
-      datemodified: "2022-02-14 00:00:00",
-      datepublished: "2022-02-14 00:00:00",
-    });
-  });*/
-
   return (
     <>
       <div className="NormalEditor">
         <div className="background">
-          <button className="button-usetemplate" onClick={clicked}>
+          <a className="button-usetemplate" href="/template-editor">
             Use Template
-          </button>
+          </a>
           <h3 className="h3">Title </h3>
           <form>
             <input
@@ -110,6 +61,7 @@ const NormalEditor = () => {
           <img
             className="spell-icon"
             src={process.env.PUBLIC_URL + "/images/spellCheck.png"}
+            alt="spelling checker"
           />
           <div className="editor">
             <h3 className="h3">Body Content</h3>

c6f1fe26f08071b25f40d32bd6aa4c879f960d95
Nisa <nurulanisah.mohamada@petronas.com>
2022-02-18 10:56:51 +0800

    Add route for Admin Nav Bar

--- Application/src/services/ProtectedRoute/index.js
+++ Application/src/services/ProtectedRoute/index.js
@@ -1,81 +1,102 @@
-import React, { useEffect, useState } from 'react';
-import { Route, Redirect } from 'react-router-dom';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { Route, Redirect } from "react-router-dom";
+import axios from "axios";
 
-const ProtectedRoute = ({component:Component, ...rest}) => {
-    const url = Object.values({ ...rest.location.pathname }).join("");
-    const tokenEmail = localStorage.getItem("user_email");
-    const isAuthenticated = localStorage.getItem("id_token");
-    const [users,setUsers] = useState([]);
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+  const url = Object.values({ ...rest.location.pathname }).join("");
+  const tokenEmail = localStorage.getItem("user_email");
+  const isAuthenticated = localStorage.getItem("id_token");
+  const [users, setUsers] = useState([]);
 
-    useEffect(() => {
-        axios.get("http://localhost:8080/user/sel")
-        .then((res) => {
-            if (res.status === 200) setUsers(res.data[0]);
-        })
-        .catch((err) => console.log(err));
-    }, []);
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/user/sel")
+      .then((res) => {
+        if (res.status === 200) setUsers(res.data[0]);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-    const getRole = () => {
-        let role = "";
-        users.map((user) => {
-            if(user.Email === tokenEmail)
-                role=user.Name;
-        });
-        return role;
-    }
+  const getRole = () => {
+    let role = "";
+    users.map((user) => {
+      if (user.Email === tokenEmail) role = user.Name;
+    });
+    return role;
+  };
 
-    const userRole = getRole();
-    
-    const checkAdmin = () => {
-        return (userRole === "System Admin") 
-    }
+  const userRole = getRole();
 
-    localStorage.setItem("isAdmin", (checkAdmin() ? "true":"false"))
+  const checkAdmin = () => {
+    return (
+      userRole !== "End User" &&
+      userRole !== "Content Contributor" &&
+      userRole !== "Content Approver"
+    );
+  };
 
-    //check whether id token is generated or not
-    if(!isAuthenticated) {
-        return <Redirect to ="/" />
-    } else if(url.indexOf("/creator") > -1){
-            if(userRole !== "End User" && userRole !== "Content Approver" && userRole !== "System Admin") {
-                return <Route {...rest} />
-            } else {
-                return <Redirect push to="/homepage" />
-            }
-    } else if(url.indexOf("/approver") > -1){
-        if(userRole !== "End User" && userRole !== "System Admin" && userRole !== "Content Contributor") {
-            return <Route {...rest} />
-        } else {
-            return <Redirect push to="/homepage" />
-        }
-    } else if(url.indexOf("/admin") > -1){
-        if(userRole !== "End User" && userRole !== "Content Approver" && userRole !== "Content Contributor") {
-            return <Route {...rest} />
-        } else {
-            return <Redirect push to="/homepage" />
-        }
-    } else if(url.indexOf("/editor") > -1){
-        if(userRole !== "End User" && userRole !== "System Admin") {
-            return <Route {...rest} />
-        } else {
-            return <Redirect push to="/homepage" />
-        }
-    } else if(url.indexOf("/content") > -1){
-        if(userRole === "System Admin") {
-            return <Redirect to="/admin/home" />
-        } else {
-            return <Route {...rest} />
-        }
-    } else if(url.indexOf("/homepage") > -1) {
-        if(userRole === "System Admin") {
-            return <Redirect push to= "/admin/home" />
-        } else {
-            return <Route {...rest} />
-        }
+  localStorage.setItem("isAdmin", checkAdmin() ? "true" : "false");
+
+  console.log(userRole);
+
+  //check whether id token is generated or not
+  if (!isAuthenticated) {
+    return <Redirect to="/" />;
+  } else if (url.indexOf("/creator") > -1) {
+    if (
+      userRole !== "End User" &&
+      userRole !== "Content Approver" &&
+      userRole !== "System Admin"
+    ) {
+      return <Route {...rest} />;
     } else {
-        return <Route {...rest} />
+      return <Redirect push to="/homepage" />;
     }
-    
-}
+  } else if (url.indexOf("/approver") > -1) {
+    if (
+      userRole !== "End User" &&
+      userRole !== "System Admin" &&
+      userRole !== "Content Contributor"
+    ) {
+      return <Route {...rest} />;
+    } else {
+      return <Redirect push to="/homepage" />;
+    }
+  } else if (url.indexOf("/admin") > -1) {
+    if (
+      userRole !== "End User" &&
+      userRole !== "Content Approver" &&
+      userRole !== "Content Contributor"
+    ) {
+      return <Route {...rest} />;
+    } else {
+      return <Redirect push to="/homepage" />;
+    }
+  } else if (url.indexOf("/editor") > -1) {
+    if (userRole !== "End User" && userRole !== "System Admin") {
+      return <Route {...rest} />;
+    } else {
+      return <Redirect push to="/homepage" />;
+    }
+  } else if (url.indexOf("/content") > -1) {
+    if (userRole === "System Admin") {
+      return <Redirect to="/admin/home" />;
+    } else {
+      return <Route {...rest} />;
+    }
+  } else if (url.indexOf("/homepage") > -1) {
+    if (
+      userRole !== "End User" &&
+      userRole !== "Content Contributor" &&
+      userRole !== "Content Approver"
+    ) {
+      return <Redirect push to="/admin/home" />;
+    } else {
+      return <Route {...rest} />;
+    }
+  } else {
+    return <Route {...rest} />;
+  }
+};
 
-export default ProtectedRoute
+export default ProtectedRoute;

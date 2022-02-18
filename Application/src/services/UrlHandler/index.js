374e77fcb425545a560e2aa9781916290488866a
Mohammad Amirul Luqman Shamshi <m.miruluqman@gmail.com>
2022-02-18 12:49:35 +0800

    fix; change to Link for return statement

--- Application/src/services/UrlHandler/index.js
+++ Application/src/services/UrlHandler/index.js
@@ -1,21 +1,13 @@
 import React, { useEffect } from 'react';
-import { Redirect, useHistory } from "react-router-dom";
-
+import { Link, useHistory } from "react-router-dom";
 const Urlhandler = () => {
-    let history = useHistory();
+    const history = useHistory();
     useEffect(() => {
-        alert("URL is not found, either it is misstyped or URL is not defined yet")
+        alert("URL is not found, either it is misstyped or URL is not defined yet");
     })
- 
-    const prevPage = () => {
-        history.goBack();
-    }
-    console.log(history.goBack())
-    return(
-        <div onLoad={() => prevPage()}>
 
-        </div>
+    return(
+        <Link to={history.goBack()}/>
     )
 }
-
 export default Urlhandler;

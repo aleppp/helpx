bbf203a1fb55e2144402a1f0309a71bb6131917c
Alif Muqri Hazmi <alifmuqri.hazmi@petronas.com>
2022-02-23 15:55:26 +0800

    Add function slider for edit and delete in list of application table

--- Application/src/components/Admin/AdminConfiguration/ActionButtonsConfig/DeleteApplication/DeleteApplicationList.js
+++ Application/src/components/Admin/AdminConfiguration/ActionButtonsConfig/DeleteApplication/DeleteApplicationList.js
@@ -1,49 +1,38 @@
 import React from "react";
 import "../style.css";
-import Buttons from "../../../Buttons/Buttons";
 
-export default function DeleteApplicationList() {
-  const button = [
-    {
-      type: "button-blue-small",
-      text: "Delete",
-    },
-    {
-      type: "button-red",
-      text: "Cancel",
-    },
-  ];
-
+export default function DeleteApplicationList({ appList, ...props }) {
   return (
-    <div class="container-deleteapp">
-      <p className="title-deleteapp">Delete Application</p>
-      <br />
-      <label className="float-left" for="question">
-        Application Name
-      </label>
-      <textarea
-        className="textarea-question-del"
-        name="question"
-        placeholder="Enter an answer"
-      >
-        Alpha Oil
-      </textarea>
-      <label className="float-left" for="answer">
-        Application URL
-      </label>
-      <textarea
-        className="textarea-answer-del"
-        name="answer"
-        placeholder="Enter an answer."
-      >
-        petronas.com/AlphaOil
-      </textarea>
-      <br />
-      <br />
-      <div className="float-center">
-        <p>Confirm Delete?</p>
-        <Buttons button={button[0]}></Buttons>
-        <Buttons button={button[1]}></Buttons>
+    <div className="container-lg" id="deleteapp">
+      <div className="row">
+        <div className="row mb-5 ps-5 mt-5">
+          <h1 className="text-center fs-5">Delete Application</h1>
+        </div>
+        <div className="row-9 mb-5">
+          <form>
+            <label className="float-start">Application Name</label>
+            <input type="text" placeholder={appList.name} disabled />
+            <label className="float-start">Application Name</label>
+            <input type="text" placeholder={appList.url} disabled />
+          </form>
+        </div>
+
+        <div className="row float-center">
+          <h1 className="text-center fs-6 ">Confirm Delete?</h1>
+        </div>
+        <div className="row mb-5">
+          <div className="col">
+            <button className="button-blue-small">Confirm</button>
+          </div>
+          <div className="col">
+            <button
+              className="button-red"
+              onClick={() => props.changeState(false)}
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- Application/src/components/Admin/AdminConfiguration/ActionButtonsConfig/EditApplication/EditApplicationList.js
+++ Application/src/components/Admin/AdminConfiguration/ActionButtonsConfig/EditApplication/EditApplicationList.js
@@ -1,32 +1,41 @@
+import React, { useState } from "react";
+import axios from "axios";
+
 import "../style.css";
 
 export default function EditListOfApp({ appList, ...props }) {
+  const [appName, setAppName] = useState([]);
+  const [appUrl, setAppUrl] = useState([]);
+
   return (
-    <div class="container-editapp-1">
-      <h2 className="title-editapp">Edit Application</h2>
-      <br></br>
-      <div className="display-column">
-        <label className="float-left">Application Name</label>
-        <textarea
-          className="textarea-app"
-          name="question"
-          placeholder="Apha Oil"
-        ></textarea>
-        <br></br>
-        <label className="float-left" for="answer">
-          Application URL
-        </label>
-        <textarea
-          className="textarea-app"
-          name="answer"
-          placeholder="petronas.com/AlphaOil"
-        ></textarea>
-      </div>
-      <div className="float-center-app">
-        <button className="button-blue-small">Save</button>
-        <button className="button-red" onClick={() => props.changeState(false)}>
-          Cancel
-        </button>
+    <div className="container-lg" id="editapp">
+      <div className="row">
+        <div className="row mb-5 ps-5 mt-5">
+          <h1 className="text-center fs-5">Edit Application</h1>
+        </div>
+        <div className="row-9">
+          <form>
+            <div className="form-group">
+              <label className="float-start">Application Name</label>
+              <input type="text" placeholder={appList.name} />
+              <label className="float-start">Application URL</label>
+              <textarea id="textarea-app" placeholder={appList.url}></textarea>
+            </div>
+          </form>
+        </div>
+        <div className="row float-center mb-5">
+          <div className="col">
+            <button className="button-blue-small">Save</button>
+          </div>
+          <div className="col">
+            <button
+              className="button-red"
+              onClick={() => props.changeState(false)}
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
